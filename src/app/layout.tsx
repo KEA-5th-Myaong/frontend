9ab5d899865dc84671a96b714485b5d7b9e7bb4b
kimgorok <kjyse2@naver.com>
2024-09-15 18:50:35 +0900

    style: 💄폰트 세팅
    - 기본 폰트로 Pretendard를 적용
    
    기본 설정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,13 @@
 import type { Metadata } from 'next';
 import './globals.css';
+import localFont from 'next/font/local';
+
+const pretendard = localFont({
+  src: 'fonts/PretendardVariable.woff2',
+  display: 'swap',
+  weight: '45 920',
+  variable: '--font-pretendard',
+});
 
 export const metadata: Metadata = {
   title: 'Popolog',
@@ -13,7 +21,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ko">
-      <body>{children}</body>
+      <body className={`${pretendard.variable} font-pretendard`}>{children}</body>
     </html>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,9 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        pretendard: ['var(--font-pretendard)'],
+      },
       colors: {
         background: 'var(--background)',
         foreground: 'var(--foreground)',
