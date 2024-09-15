7f05d81cbe0995901cd92fac06e8dd512a2b2f9b
kimgorok <kjyse2@naver.com>
2024-09-15 23:50:25 +0900

    design: 💄tailwindcss 세팅
    - tailwind.config.ts 파일에서 tailwindcss 속성을 추가
    - globals.css 파일에서 기본 클래스 세팅
    - gitmessage.txt 파일에서 이모지를 변경했습니다.
    
    기본 설정

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,13 +7,26 @@ const config: Config = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    screens: {
+      sm: '576px', // 가로 px이 각각 576px, 768px, 882px 이상일 때의 스타일을 지정
+      md: '768px',
+      lg: '882px',
+    },
     extend: {
       fontFamily: {
         pretendard: ['var(--font-pretendard)'],
       },
       colors: {
-        background: 'var(--background)',
-        foreground: 'var(--foreground)',
+        gray: {
+          0: '#F4F4F4',
+          1: '#E0E0E0',
+          2: '#B8B8B8',
+          3: '#929292',
+          4: '#6E6E6E',
+          5: '#4B4B4B',
+          6: '#2B2B2B',
+          7: '#111111',
+        },
       },
     },
   },
