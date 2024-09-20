4d3a9ea920ef9ccf9c0e2897b2dd7557522b0176
kimgorok <kjyse2@naver.com>
2024-09-20 18:45:06 +0900

    💄design: [로그인] 로그인 페이지 UI수정
    - 변경된 figma 디자인에 맞게 수정
    
    /login

--- src/app/(route)/(Login_SignUp)/login/_container/LogInContainer.tsx
+++ src/app/(route)/(Login_SignUp)/login/_container/LogInContainer.tsx
@@ -6,23 +6,21 @@ export default function LogInContainer() {
   return (
     <div className="flex-center flex-col self-stretch">
       <div className="flex flex-col items-center w-1/3">
-        <p className="mb-6 text-3xl font-bold text-center">{FORM_TEXT[0]}</p>
+        <p className="mb-8 text-[22px] font-semibold text-center">{FORM_TEXT[0]}</p>
         <LoginForm />
 
         <div className="flex self-stretch justify-around mt-6">
-          <Link href="/" className="">
+          <Link href="/" className="text-xs">
             {FORM_TEXT[3]}
           </Link>
 
-          <Link href="/sign-up" className="">
+          <Link href="/sign-up" className="text-xs">
             {FORM_TEXT[4]}
           </Link>
         </div>
 
-        <div className="w-1/2 my-6 mx-auto bg-gray-1 h-[2px]" />
-
-        <div className="flex flex-col self-stretch items-center">
-          <span className="text-lg font-semibold mb-4">간편로그인</span>
+        <div className="flex flex-col self-stretch items-center mt-8">
+          <span className="text-xs mb-4 text-gray-3">SNS로 바로 시작하기</span>
 
           <div className="flex gap-12">
             <div className="bg-yellow-300 rounded-full p-6 cursor-pointer" />
