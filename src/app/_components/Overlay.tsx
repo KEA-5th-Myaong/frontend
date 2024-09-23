56b5963b60a37996ed006ea333d0faf463160ac1
kimgorok <kjyse2@naver.com>
2024-09-24 00:06:52 +0900

    💄design: [공통] 오버레이 배경 뿌옇게 추가
    - Overlay.tsx에 뿌연 효과 추가
    
    /

--- src/app/_components/Overlay.tsx
+++ src/app/_components/Overlay.tsx
@@ -16,7 +16,7 @@ export default function Overlay({ children, onClick }: { children: React.ReactNo
     <motion.div
       onClick={onClick}
       {...overlayMotion}
-      className="fixed inset-0 flex-center z-50 bg-black-3 bg-opacity-25"
+      className="fixed inset-0 flex-center bg-black-3 bg-opacity-25 z-50 backdrop-blur-[3px]"
     >
       {children}
     </motion.div>
