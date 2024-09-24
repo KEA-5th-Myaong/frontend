d907f242bf98554462fe2c3fb736b7b1293e920f
kimgorok <kjyse2@naver.com>
2024-09-24 14:51:35 +0900

    ✨feat: [로그인/회원가입/비밀번호찾기] input type='password'에 눈 표시 적용
    - type이 password인 inputdp 눈 표시를 적용
    - 눈 모양을 누르면 type이 바뀌게 설정
    
    /log-in, /sign-up, /chang-pwd

--- src/app/(route)/(Login_SignUp)/_components/FormInput.tsx
+++ src/app/(route)/(Login_SignUp)/_components/FormInput.tsx
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FieldValues, Path } from 'react-hook-form';
 import { FormInputProps } from '../_types/forms';
+import Icons from '../../../_components/ui/Icon';
+import { EyeIcon, EyeSlashIcon } from '../../../_components/ui/iconPath';
 
 export default function FormInput<T extends FieldValues>({
   id,
@@ -16,6 +18,12 @@ export default function FormInput<T extends FieldValues>({
   minLength = 1,
   infoText = '',
 }: FormInputProps<T>) {
+  const [showPassword, setShowPassword] = useState(false);
+  const isPassword = type === 'password';
+
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+  };
   return (
     <div className="relative">
       <div className="flex items-center gap-2">
@@ -24,13 +32,21 @@ export default function FormInput<T extends FieldValues>({
       <input
         maxLength={maxLength}
         minLength={minLength}
-        type={type}
+        type={isPassword ? (showPassword ? 'text' : 'password') : type}
         id={id as string}
         {...register(id as Path<T>, { required, onBlur })}
         className="mt-2 form-input"
         placeholder={placeholder}
       />
-
+      {isPassword && (
+        <button
+          type="button"
+          onClick={togglePasswordVisibility}
+          className="absolute right-4 top-[62px] transform -translate-y-1/2"
+        >
+          <Icons name={showPassword ? EyeIcon : EyeSlashIcon} />
+        </button>
+      )}
       {error ? (
         <p className="form-error-text">{error.message}</p>
       ) : (
