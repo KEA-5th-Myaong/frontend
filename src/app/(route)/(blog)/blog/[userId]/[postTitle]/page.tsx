7e5d963304a7393911740088c07c7e9b9baca340
kimgorok <kjyse2@naver.com>
2024-10-01 00:48:15 +0900

    🎉init: [블로그] KAN-82 포스트 상세 조회 페이지 세팅
    - /blog/[userId]/[postTitle] 라우팅
    
    /blog/[userId]/[postTitle]

---
+++ src/app/(route)/(blog)/blog/[userId]/[postTitle]/page.tsx
@@ -0,0 +1,16 @@
+import PostContent from './_components/PostContent';
+import PostComment from './_components/PostComment';
+
+export default function BlogPosts() {
+  return (
+    <section className="flex justify-center pt-[100px] pb-12">
+      <div className="w-full min-w-[375px] max-w-[1000px] px-11 sm:px-[108px]">
+        {/* 포스트 컨텐츠 */}
+        <PostContent />
+
+        {/* 댓글 */}
+        <PostComment />
+      </div>
+    </section>
+  );
+}
