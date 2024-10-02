6157c321d588e8b56b4e5186d55605237b928648
kimgorok <kjyse2@naver.com>
2024-10-02 15:59:11 +0900

    ✨feat: [모의면접] 기업 선택 페이지 레이아웃 세팅
    - 돋보기 아이콘 추가 및 적용
    - globals.css 커스텀 클래스 추가
    - 페이지 세팅 및 반응형 적용
    
    KAN-85

--- src/app/(route)/(interview)/interview/[id]/select/page.tsx
+++ src/app/(route)/(interview)/interview/[id]/select/page.tsx
@@ -1,9 +1,63 @@
-import InterviewHistory from '../../../_components/InterviewHistory';
+'use client';
+
+import { useState } from 'react';
+import { useParams, useRouter } from 'next/navigation';
+import Icons from '../../../../../_components/ui/Icon';
+import { SearchIcon } from '../../../../../_components/ui/iconPath';
 
 export default function InterviewSelect() {
+  const router = useRouter();
+  const id = useParams();
+
+  const [corpList, setCorpList] = useState([
+    '위볼린',
+    '텐핑거스',
+    '로그앤코딩',
+    '조아라',
+    '위볼린',
+    '텐핑거스',
+    '로그앤코딩',
+    '조아라',
+    '위볼린',
+    '텐핑거스',
+    '로그앤코딩',
+    '조아라',
+    '위볼린',
+    '텐핑거스',
+    '로그앤코딩',
+    '조아라',
+    '위볼린',
+    '텐핑거스',
+    '로그앤코딩',
+    '조아라',
+  ]);
   return (
-    <section className="flex justify-center pt-[100px] pb-12">
-      <InterviewHistory />
+    <section className="w-full pl-0 sm:pl-7 md:pl-16 lg:pl-20 xl:pl-32 pt-11">
+      <p className="font-semibold">모의 면접</p>
+
+      <div className="flex gap-4 self-stretch pt-7">
+        <div className="flex self-stretch items-center px-5 py:1 md:py-4 gap-5 rounded-[3rem] border border-primary-1 w-full sm:w-[320px] lg:w-full">
+          <Icons name={SearchIcon} />
+          <input className="w-full focus:outline-none" placeholder="기업 이름을 검색하거나 선택하세요" />
+        </div>
+        <button type="button" className="px-2 md:px-10 py-1 md:py-[18px] rounded-[2rem] primary-1-btn">
+          선택
+        </button>
+      </div>
+
+      <div className="flex flex-wrap pt-5">
+        {corpList.map((corp) => (
+          <button
+            type="button"
+            onClick={() => {
+              router.push(`/interview/${id.id}/${corp}`);
+            }}
+            className="corp-block"
+          >
+            {corp}
+          </button>
+        ))}
+      </div>
     </section>
   );
 }
