36d81722bb00767de4a033bf021d2f36f385c06d
kimgorok <kjyse2@naver.com>
2024-10-08 12:05:01 +0900

    💄design: [블로그] 팔로잉 버튼 추가
    - 임시 팔로잉 버튼 추가
    
    KAN-82

--- src/app/(route)/(blog)/blog/[userId]/_components/_follow/FollowModal.tsx
+++ src/app/(route)/(blog)/blog/[userId]/_components/_follow/FollowModal.tsx
@@ -36,9 +36,17 @@ export default function FollowModal({ isOpen, onClose, title, list }: FollowModa
 
         <div className="flex flex-col gap-6 w-full h-[514px] overflow-scroll pb-2 hide-scrollbar">
           {list.map((followList) => (
-            <div key={followList.id} className="flex items-center gap-5">
-              <div className="w-[52px] h-[52px] min-w-[52px] max-h-[52px] bg-purple-300 rounded-full " />
-              <p className="font-medium text-lg whitespace-nowrap text-ellipsis">{followList.name}</p>
+            <div key={followList.id} className="flex items-center justify-between">
+              <div className="flex items-center gap-5">
+                <div className="w-[52px] h-[52px] min-w-[52px] max-h-[52px] bg-purple-300 rounded-full " />
+                <p className="font-medium min-w-[180px] max-w-[240px] text-lg overflow-hidden text-ellipsis whitespace-nowrap flex-grow">
+                  {followList.name}
+                </p>
+              </div>
+
+              <button type="button" className="text-xl font-semibold primary-1-btn py-2 px-6 rounded-[10px]">
+                팔로잉
+              </button>
             </div>
           ))}
         </div>
