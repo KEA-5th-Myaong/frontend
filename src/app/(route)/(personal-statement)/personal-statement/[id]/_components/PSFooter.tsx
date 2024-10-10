8f3f727a1269901e626d0bfe7dc5910dd7a6034d
kimgorok <kjyse2@naver.com>
2024-10-10 19:19:18 +0900

    ✨feat: [자소서] 자소서 푸터 세팅
    - 자소서 생성, 조회에서 사용되는 푸터 세팅
    
    KAN-106

---
+++ src/app/(route)/(personal-statement)/personal-statement/[id]/_components/PSFooter.tsx
@@ -0,0 +1,31 @@
+interface PSFooterProps {
+  handlePdfClick: () => void;
+  handlePreviewClick: () => void;
+  handleDoneClick: () => void;
+}
+
+export default function PSFooter({ handlePdfClick, handlePreviewClick, handleDoneClick }: PSFooterProps) {
+  return (
+    <div
+      className="fixed bottom-0 w-full flex items-center justify-between bg-white-0 py-4 px-16 
+    border border-t-gray-2 font-semibold whitespace-nowrap"
+    >
+      <button type="button" onClick={handlePdfClick} className="rounded-[10px] border border-[#E1E1E1] py-2 px-6">
+        PDF로 저장
+      </button>
+
+      <div className="flex gap-5">
+        <button type="button" onClick={handlePreviewClick} className="rounded-[10px] border border-[#E1E1E1] py-2 px-6">
+          미리보기
+        </button>
+        <button
+          type="button"
+          onClick={handleDoneClick}
+          className="bg-primary-1 text-white-0 rounded-[10px] border border-[#E1E1E1] py-2 px-6"
+        >
+          작성 완료
+        </button>
+      </div>
+    </div>
+  );
+}
--- src/app/(route)/(personal-statement)/personal-statement/[id]/read/page.tsx
+++ src/app/(route)/(personal-statement)/personal-statement/[id]/read/page.tsx
@@ -1,3 +1,10 @@
+import PSFooter from '../_components/PSFooter';
+
 export default function PersonalStatementRead() {
-  return <div>자소서 조회</div>;
+  return (
+    <div>
+      자소서 조회
+      <PSFooter handlePdfClick={() => {}} handlePreviewClick={() => {}} handleDoneClick={() => {}} />
+    </div>
+  );
 }
