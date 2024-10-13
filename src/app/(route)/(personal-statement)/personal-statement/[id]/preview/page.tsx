5fe31b4426729564a9970e59ab39848e9353b29b
kimgorok <kjyse2@naver.com>
2024-10-13 18:28:05 +0900

    ✨feat: [자소서] PDF로 저장 버튼 구현
    - react-to-pdf를 설치하여 PDF로 저장 구현
    - 디자인 추후에 수정해야함
    
    KAN-106

--- src/app/(route)/(personal-statement)/personal-statement/[id]/preview/page.tsx
+++ src/app/(route)/(personal-statement)/personal-statement/[id]/preview/page.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { useRouter } from 'next/navigation';
+import { usePDF } from 'react-to-pdf';
 import PSFooter from '../_components/PSFooter';
 import usePSStore from '../_store/psStore';
 import PSCreateHeader from '../create/_components/PSCreateHeader';
@@ -10,8 +11,17 @@ export default function PersonalStatementPreview() {
   const router = useRouter();
   const psData = usePSStore((state) => state.psData);
 
+  const { toPDF, targetRef } = usePDF({
+    filename: '자기소개서.pdf',
+    page: { format: 'A4' },
+    method: 'save',
+  });
+
   return (
-    <section className="flex-center flex-col mx-auto w-full h-full pt-[100px] pb-32 px-8 gap-20 max-w-[1000px] min-w-[365px]">
+    <section
+      ref={targetRef}
+      className="flex-center flex-col mx-auto w-full h-full pt-[100px] pb-32 px-8 gap-20 max-w-[1000px] min-w-[365px]"
+    >
       <PSCreateHeader
         title={psData.title}
         mode="preview"
@@ -31,7 +41,7 @@ export default function PersonalStatementPreview() {
       <PSFooter
         showPDF
         showBack
-        handlePdfClick={() => {}}
+        handlePdfClick={toPDF}
         handleBackClick={() => {
           router.back();
         }}
--- src/app/(route)/(personal-statement)/personal-statement/[id]/read/_components/PSReadContainer.tsx
+++ src/app/(route)/(personal-statement)/personal-statement/[id]/read/_components/PSReadContainer.tsx
@@ -2,6 +2,7 @@
 
 import { useEffect, useState } from 'react';
 import { useRouter } from 'next/navigation';
+import { usePDF } from 'react-to-pdf';
 import PSFooter from '../../_components/PSFooter';
 import PSReadContent from './PSReadContent';
 import { PSFormData } from '../../create/_types/psCreate';
@@ -47,6 +48,12 @@ export default function PSReadContainer() {
     }));
   };
 
+  const { toPDF, targetRef } = usePDF({
+    filename: '자기소개서.pdf',
+    page: { format: 'A4' },
+    method: 'save',
+  });
+
   return (
     <>
       <BackButton className="self-start pb-4" />
@@ -59,7 +66,7 @@ export default function PSReadContainer() {
         handleDeleteClick={handleDeleteClick}
       />
 
-      <div className="self-start w-full mt-20">
+      <div ref={targetRef} className="self-start w-full mt-20">
         <p className="w-full font-semibold text-[28px] pb-5 mb-9 border-b-2 border-gray-[#D9D9D9]">자기소개서</p>
 
         <div className="flex flex-col gap-11">
@@ -71,7 +78,7 @@ export default function PSReadContainer() {
       <PSFooter
         showPDF
         showBack
-        handlePdfClick={() => {}}
+        handlePdfClick={toPDF}
         handleBackClick={() => {
           router.back();
         }}
