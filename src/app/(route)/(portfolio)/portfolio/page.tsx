4573c2095d46e5b3cf10d7e6ffcf3cc87746712a
yeonilil <gwagseoyeon@gmail.com>
2024-10-15 00:24:19 +0900

    💄 style: 반응형 디자인

--- src/app/(route)/(portfolio)/portfolio/page.tsx
+++ src/app/(route)/(portfolio)/portfolio/page.tsx
@@ -12,8 +12,8 @@ export default function PortfolioList() {
   const { portfolio } = usePortfolioStore();
 
   return (
-    <div>
-      <div className="mt-[60px] flex justify-between items-center w-full max-w-[1000px] mx-auto">
+    <div className="px-[50px] md:px-0">
+      <div className="mt-[60px] flex justify-between items-center w-full max-w-[1000px] md:px-[60px] lg:px-0 lg:mx-auto">
         <div className="flex flex-col">
           <h1 className="font-semibold text-left text-[16px]">포트폴리오 관리</h1>
           <p className="text-left text-gray-0 text-[12px]">최대 5개까지 생성 가능합니다</p>
@@ -21,7 +21,7 @@ export default function PortfolioList() {
         <Link href="/portfolio/write">
           <button
             type="button"
-            className="flex items-center font-bold text-white-0 text-[16px] py-[19px] px-[28px] bg-primary-4 rounded-[30px] hover-animation"
+            className="flex items-center font-bold text-white-0 text-[16px] py-[13px] md:py-[19px] px-[20px] md:px-[28px] bg-primary-4 rounded-[30px] hover-animation"
           >
             <Icons
               className="mr-[10px]"
@@ -31,10 +31,10 @@ export default function PortfolioList() {
           </button>
         </Link>
       </div>
-      <div className="flex flex-col items-center">
-        <div className="w-full max-w-[1000px] grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-[20px] gap-y-[25px] mt-[30px] mx-auto">
+      <div className="flex flex-col items-center mt-[30px] mb-[100px] px-[50px]">
+        <div className="w-full lg:max-w-[1000px] grid justify-center sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-[20px] gap-y-[25px] gap-4">
           {portfolio.map((item: PortfolioCardProps) => (
-            <PortfolioCard key={item.id} id={item.id} title={`곽서연 포트폴리오 ${item.id}`} date="2024.10.17" />
+            <PortfolioCard key={item.id} id={item.id} title={item.title} date="2024.10.17" />
           ))}
           <PortfolioAddCard />
         </div>
