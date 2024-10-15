7f1b5f6519f1455e3cc6680bb8f42be24a6a8238
yeonilil <gwagseoyeon@gmail.com>
2024-10-15 20:42:20 +0900

    ✨ feat: 항목 items로 분리하여 항목 추가 기능

--- src/app/(route)/(portfolio)/portfolio/write/_components/section/EducationSection.tsx
+++ src/app/(route)/(portfolio)/portfolio/write/_components/section/EducationSection.tsx
@@ -1,12 +1,21 @@
+'use client';
+
+import { useState } from 'react';
 import Image from 'next/image';
-import Input from '../Input';
+import EducationItem from '../items/EducationItem';
 
 export default function EducationSection() {
+  const [educationItems, setEducationItems] = useState([<EducationItem key={0} />]);
+
+  const addEducationItem = () => {
+    setEducationItems([...educationItems, <EducationItem key={educationItems.length} />]);
+  };
+
   return (
     <div className="mt-5">
       <div className="flex justify-between items-center">
         <h1 className="pre-3xl-semibold">학력</h1>
-        <button type="button" className="flex-center text-[16px] hover:text-primary-4 ">
+        <button type="button" onClick={addEducationItem} className="flex-center text-[16px] hover:text-primary-4 ">
           <Image
             src="/assets/add-button.svg"
             alt="학력 추가"
@@ -18,31 +27,7 @@ export default function EducationSection() {
         </button>
       </div>
       <div className="h-[2px] w-full bg-gray-5 my-[20px]" />
-      <section className="w-full py-[20px] px-[30px] bg-gray-4 rounded-[10px]">
-        <div className="grid grid-flow-col justify-stretch gap-[20px]">
-          <Input
-            element="input"
-            label="학교명"
-            size="lg"
-            type="text"
-            color="white"
-            placeholder="학교명을 입력해주세요"
-            required
-          />
-          <Input
-            element="input"
-            label="학과"
-            size="lg"
-            type="text"
-            color="white"
-            placeholder="학과를 입력해주세요"
-            required
-          />
-        </div>
-        <Input element="input" label="졸업 일자" size="lg" type="date" color="white" required />
-        {/* //학점 형식 추가 필요 */}
-        <Input element="input" label="학점" size="lg" type="text" color="white" placeholder="학점/기준학점" />
-      </section>
+      {educationItems}
     </div>
   );
 }
--- src/app/(route)/(portfolio)/portfolio/write/_components/section/LinksSection.tsx
+++ src/app/(route)/(portfolio)/portfolio/write/_components/section/LinksSection.tsx
@@ -1,7 +1,16 @@
+'use client';
+
+import { useState } from 'react';
 import Image from 'next/image';
-import Input from '../Input';
+import LinkItem from '../items/LinkItem';
 
 export default function LinksSection() {
+  const [linkItems, setLinkItems] = useState([<LinkItem key={0} />]);
+
+  const addLinkItem = () => {
+    setLinkItems([...linkItems, <LinkItem key={linkItems.length} />]);
+  };
+
   return (
     <div className="mt-5">
       <div className="flex justify-between items-center">
@@ -9,25 +18,14 @@ export default function LinksSection() {
       </div>
       <div className="h-[2px] w-full bg-gray-5 my-[20px]" />
       <div className="flex flex-col items-center">
-        <section className="w-full py-[20px] px-[30px] bg-gray-4 rounded-[10px]">
-          <div className="grid grid-flow-col justify-stretch gap-[20px]">
-            <Input
-              element="input"
-              label="링크명"
-              size="lg"
-              type="text"
-              color="white"
-              placeholder="링크명을 입력해주세요"
-            />
-            <Input element="input" label="URL" size="lg" type="text" color="white" placeholder="URL을 입력해주세요" />
-          </div>
-        </section>
+        {linkItems}
         <Image
           src="/assets/add-button.svg"
           alt="링크 추가"
           width={40}
           height={40}
-          className=" hover-animation mr-[10px] mt-[20px]"
+          className="hover-animation mr-[10px] mt-[20px]"
+          onClick={addLinkItem}
         />
       </div>
     </div>
