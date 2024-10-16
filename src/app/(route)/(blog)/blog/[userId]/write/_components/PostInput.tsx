2c2f8dace475cb5db0ffdce3c54a2fcaee862e19
jiyeon <bjyy0201@gmail.com>
2024-10-16 14:02:10 +0900

    ✨ feat:[블로그] 작성 area
    - 임시 textarea 구성
    
    KAN-129

---
+++ src/app/(route)/(blog)/blog/[userId]/write/_components/PostInput.tsx
@@ -0,0 +1,7 @@
+export default function PostInput() {
+  return (
+    <div>
+      <textarea className="w-[920px] h-[530px] resize-none outline-none border-2 border-gray-5"></textarea>
+    </div>
+  );
+}
--- src/app/(route)/(blog)/blog/[userId]/write/page.tsx
+++ src/app/(route)/(blog)/blog/[userId]/write/page.tsx
@@ -1,13 +1,14 @@
 import BackButton from '@/app/_components/BackButton';
+import PostInput from './_components/PostInput';
 
 export default function PostWrite() {
   return (
     <div className="flex-center flex-col">
-      <div className="flex-center flex-col w-[1000px] h-[850px] ">
+      <div className="flex-center flex-col w-[1000px] h-[800px] ">
         <div className="flex w-full h-12 px-5 mb-2">
           <BackButton />
         </div>
-        <div className="flex-col w-full h-[100px] px-10">
+        <div className="flex-col w-full h-[120px] px-10">
           <div className="my-1">
             <p>포스트 작성</p>
           </div>
@@ -15,13 +16,21 @@ export default function PostWrite() {
             <input
               type="text"
               placeholder="제목을 입력해주세요"
-              className="text-2xl bg-inherit outline-none w-full"
+              className="text-3xl bg-inherit font-semibold outline-none w-full"
               maxLength={42}
             />
           </div>
         </div>
-        <div>
-          <div className="flex w-full h-[500px] px-10"></div>
+        <div className="flex-center w-full h-[550px] px-10">
+          <PostInput />
+        </div>
+        <div className="relative w-[920px] h-11">
+          <button
+            type="button"
+            className="absolute items-center justify-center w-32 h-12 text-sm rounded-3xl bg-primary-1 text-white-0  right-0"
+          >
+            작성 완료
+          </button>
         </div>
       </div>
     </div>
