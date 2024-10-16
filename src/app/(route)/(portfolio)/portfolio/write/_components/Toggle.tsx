3c7d0dc498e40a05e21af295605de03fca7625ab
yeonilil <gwagseoyeon@gmail.com>
2024-10-16 19:08:26 +0900

    ✨ feat: 항목 편집 구현

--- src/app/(route)/(portfolio)/portfolio/write/_components/Toggle.tsx
+++ src/app/(route)/(portfolio)/portfolio/write/_components/Toggle.tsx
@@ -1,23 +1,20 @@
-import { useState } from 'react';
+interface ToggleProps {
+  onToggle: () => void;
+  isChecked: boolean;
+}
 
-export default function Toggle() {
-  const [isOn, setIsOn] = useState<boolean>(false);
-
-  const toggleHandler = (): void => {
-    setIsOn((prev) => !prev);
-  };
-
+export default function Toggle({ onToggle, isChecked }: ToggleProps) {
   return (
     <div className="flex flex-col items-center ">
       <button
         type="button"
-        onClick={toggleHandler}
-        className={` ${isOn ? 'bg-green-500' : 'bg-gray-300'} relative cursor-pointer w-10 h-6 rounded-full transition duration-500`}
+        onClick={onToggle}
+        className={` ${isChecked ? 'bg-green-500' : 'bg-gray-300'} relative cursor-pointer w-10 h-6 rounded-full transition duration-500`}
       >
         {' '}
         <div
           className={`absolute top-1 left-1 w-4 h-4 bg-white-0 rounded-full transition duration-500 ${
-            isOn ? 'translate-x-4' : ''
+            isChecked ? 'translate-x-4' : ''
           }`}
         />
       </button>
