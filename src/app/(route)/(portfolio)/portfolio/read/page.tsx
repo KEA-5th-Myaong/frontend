73f213f07613ebec25e94a35461457533e4df607
yeonilil <gwagseoyeon@gmail.com>
2024-10-17 12:07:34 +0900

    ✨ feat: 포트폴리오 pdf 저장 기능 구현

--- src/app/(route)/(portfolio)/portfolio/read/page.tsx
+++ src/app/(route)/(portfolio)/portfolio/read/page.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { useRef, useState } from 'react';
+import { Margin, usePDF } from 'react-to-pdf';
 import Icons from '@/app/_components/ui/Icon';
 import PortfolioContainer from './_components/PortfolioContainer';
 import PortfolioHeader from './_components/PortfolioHeader';
@@ -21,6 +22,12 @@ export default function PortfolioRead() {
     callback: () => setIsShowDropdown(false),
   });
 
+  const { toPDF, targetRef } = usePDF({
+    filename: '포트폴리오.pdf',
+    page: { margin: Margin.SMALL, format: 'A4' },
+    method: 'save',
+  });
+
   return (
     <div className="flex px-[50px] md:px-0 mb-[100px]">
       <div className="mt-[60px]  w-full max-w-[1000px] md:px-[60px] lg:px-0 lg:mx-auto">
@@ -29,7 +36,7 @@ export default function PortfolioRead() {
         </div>
         {isLogined && <PortfolioHeader />}
 
-        <section>
+        <section ref={targetRef}>
           <div className="relative flex justify-between items-center my-10">
             <h1 className="pre-3xl-semibold">{title}</h1>
             <div ref={dropdownRef}>
@@ -44,10 +51,10 @@ export default function PortfolioRead() {
 
             {isShowDropdown && isLogined && <PortfolioWriteDropdown />}
           </div>
+          <PortfolioContainer />
         </section>
-        <PortfolioContainer />
       </div>
-      <Footer showPDF showLink />
+      <Footer showPDF showLink handlePdfClick={toPDF} />
     </div>
   );
 }
