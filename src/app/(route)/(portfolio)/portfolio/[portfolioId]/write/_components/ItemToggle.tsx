01a58142bc12eeb02b26d79acdb91ca318cd9044
yeonilil <gwagseoyeon@gmail.com>
2024-10-18 16:54:46 +0900

    🐛 fix: 항목 편집 컴포넌트 위치 수정

--- src/app/(route)/(portfolio)/portfolio/[portfolioId]/write/_components/ItemToggle.tsx
+++ src/app/(route)/(portfolio)/portfolio/[portfolioId]/write/_components/ItemToggle.tsx
@@ -6,7 +6,7 @@ import useToggleStore from '@/app/_store/portfolioToggle';
 export default function ItemToggle() {
   const { toggles, setToggle } = useToggleStore();
   return (
-    <div className="w-[250px] h-auto y-[20px] px-[30px] py-[20px] bg-white-0 rounded-[10px] border border-gray-5 ">
+    <div className="w-[300px] h-[500px] y-[20px] px-[30px] py-[20px] bg-white-0 rounded-[10px] border border-gray-5 z-10">
       <div className="px-[10px] py-[10px] font-semibold  bg-gray-4 rounded-[10px]">항목 편집</div>
       <div className="px-[10px] py-[10px] flex items-center gap-[10px]">
         <Icons name={RequiredIcon} />
--- src/app/(route)/(portfolio)/portfolio/[portfolioId]/write/page.tsx
+++ src/app/(route)/(portfolio)/portfolio/[portfolioId]/write/page.tsx
@@ -46,89 +46,89 @@ export default function PortfolioWrite() {
   };
 
   return (
-    <div className="flex px-[50px] md:px-0 mb-[100px]">
-      <div className="m-0 fixed top-[100px] left-[100px] bg-white z-10">
+    <div className="mb-[100px]">
+      <div className="flex mt-[60px] max-w-[1200px] md:px-[60px] lg:px-0 lg:mx-auto">
         <ItemToggle />
-      </div>
-      <div className="mt-[60px]  w-full max-w-[1000px] md:px-[60px] lg:px-0 lg:mx-auto">
-        <div className="flex justify-between items-center">
-          <div className="flex flex-col">
-            <h1 className="font-semibold text-left ">포트폴리오 작성</h1>
-            <p className="text-left text-gray-0 text-[12px]">최대 5개까지 생성 가능합니다</p>
+        <div className="w-full ml-10">
+          <div className="flex justify-between items-center">
+            <div className="flex flex-col">
+              <h1 className="font-semibold text-left ">포트폴리오 작성</h1>
+              <p className="text-left text-gray-0 text-[12px]">최대 5개까지 생성 가능합니다</p>
+            </div>
+            <Link href={`/portfolio/${portfolioId}/read`}>
+              <button
+                type="button"
+                className="flex items-center font-bold text-white-0 py-[13px] md:py-[19px] px-[20px] md:px-[28px] bg-primary-1 rounded-[30px] hover-animation"
+              >
+                작성 완료
+              </button>
+            </Link>
           </div>
-          <Link href={`/portfolio/${portfolioId}/read`}>
-            <button
-              type="button"
-              className="flex items-center font-bold text-white-0 py-[13px] md:py-[19px] px-[20px] md:px-[28px] bg-primary-1 rounded-[30px] hover-animation"
-            >
-              작성 완료
-            </button>
-          </Link>
-        </div>
-        <div className="relative flex justify-between items-center mt-5">
-          <h1 className="pre-3xl-semibold">{title}</h1>
-          <div ref={dropdownRef}>
-            <Icons
-              name={MoreIcon}
-              onClick={() => {
-                setIsShowDropdown((prev) => !prev);
-              }}
-              className="cursor-pointer"
-            />
-          </div>
+          <div className="relative flex justify-between items-center mt-5">
+            <h1 className="pre-3xl-semibold">{title}</h1>
+            <div ref={dropdownRef}>
+              <Icons
+                name={MoreIcon}
+                onClick={() => {
+                  setIsShowDropdown((prev) => !prev);
+                }}
+                className="cursor-pointer"
+              />
+            </div>
 
-          {isShowDropdown && <PortfolioWriteDropdown />}
-        </div>
-        <section className="mt-5 mb-[50px]">
-          <section>
-            <form>
-              <div className="grid grid-flow-col justify-stretch gap-[20px]">
+            {isShowDropdown && <PortfolioWriteDropdown />}
+          </div>
+          <section className="mt-5 mb-[50px]">
+            <section>
+              <form>
+                <div className="grid grid-flow-col justify-stretch gap-[20px]">
+                  <Input
+                    element="input"
+                    label="이름"
+                    size="lg"
+                    type="text"
+                    color="transparent"
+                    placeholder="이름을 입력해주세요"
+                    required
+                  />
+                  <Input
+                    element="input"
+                    label="휴대폰 번호"
+                    size="lg"
+                    type="tel"
+                    color="transparent"
+                    placeholder="휴대폰 번호를 입력해주세요"
+                    required
+                  />
+                </div>
                 <Input
                   element="input"
-                  label="이름"
+                  label="이메일"
+                  size="lg"
+                  type="email"
+                  color="transparent"
+                  placeholder="이메일을 입력해주세요"
+                  required
+                />
+                <Input
+                  element="input"
+                  label="관심 직무"
                   size="lg"
                   type="text"
                   color="transparent"
-                  placeholder="이름을 입력해주세요"
-                  required
+                  placeholder="관심직무를 입력해주세요"
                 />
-                <Input
-                  element="input"
-                  label="휴대폰 번호"
-                  size="lg"
-                  type="tel"
-                  color="transparent"
-                  placeholder="휴대폰 번호를 입력해주세요"
-                  required
-                />
-              </div>
-              <Input
-                element="input"
-                label="이메일"
-                size="lg"
-                type="email"
-                color="transparent"
-                placeholder="이메일을 입력해주세요"
-                required
-              />
-              <Input
-                element="input"
-                label="관심 직무"
-                size="lg"
-                type="text"
-                color="transparent"
-                placeholder="관심직무를 입력해주세요"
-              />
-            </form>
+              </form>
+            </section>
+            <EducationSection />
+            {toggles.experience && <ExperienceSection />}
+            {toggles.links && <LinksSection />}
+            {toggles.skills && <SkillsSection />}
+            {toggles.certifications && <CertificationsSection />}
+            {toggles.activities && <ActivitiesSection />}
+            {toggles.personalStatement && <PSSection />}
           </section>
-          <EducationSection />
-          {toggles.experience && <ExperienceSection />}
-          {toggles.links && <LinksSection />}
-          {toggles.skills && <SkillsSection />}
-          {toggles.certifications && <CertificationsSection />}
-          {toggles.activities && <ActivitiesSection />}
-          {toggles.personalStatement && <PSSection />}
-        </section>
+        </div>
       </div>
       <Footer showPreview showDone handlePreviewClick={handlePreviewClick} handleDoneClick={handleDoneClick} />
     </div>
