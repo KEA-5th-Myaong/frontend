03a8fc1ee860e92d4c57417be28ffd2a73d73b56
kimgorok <kjyse2@naver.com>
2024-10-19 20:35:01 +0900

    ✨feat: useCustomQuery에 defaultOptions 추가
    - 탭에 돌아오거나 마우스가 다시 들어왔을 때 api 호출 하는 것을 막는 옵션 추가
    
    KAN-142

--- src/app/_hooks/useCustomQuery.ts
+++ src/app/_hooks/useCustomQuery.ts
@@ -1,5 +1,10 @@
 import { useQuery, UseQueryOptions, QueryFunction } from '@tanstack/react-query';
 
+const defaultQueryOptions = {
+  refetchOnMount: false, // 다른 탭에 갔다오거나
+  refetchOnWindowFocus: false, // 마우스를 다시 클릭 했을 때 fetch하는거 false로
+};
+
 export default function useCustomQuery<T>(
   key: string[],
   queryFn: QueryFunction<T>,
@@ -8,6 +13,7 @@ export default function useCustomQuery<T>(
   return useQuery<T>({
     queryKey: key,
     queryFn,
+    ...defaultQueryOptions, // 기본 옵션 추가
     ...options,
   });
 }
