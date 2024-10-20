cd6832fbc208e6d1179bef11d6df28467348f4c1
kimgorok <kjyse2@naver.com>
2024-10-20 18:32:57 +0900

    ✨feat: [마이페이지] 관심직군 api 연결
    - 관심직군을 불러오는 api 연결
    - 추후 post에 queryClient.invalidatedata 연결
    - 테스트 코드 삭제
    - 타입 수정
    
    KAN-142

--- src/app/(route)/(main-page)/main/_components/InterestedJob.tsx
+++ src/app/(route)/(main-page)/main/_components/InterestedJob.tsx
@@ -6,10 +6,10 @@ import { useForm } from 'react-hook-form';
 import Image from 'next/image';
 import Overlay from '../../../../_components/Overlay';
 import { modalMotion } from '../../../../_components/Modal';
-import jobData from '../../../(my-page)/my-page/change-profile/_components/jobTest.json';
 import Icons from '@/app/_components/ui/Icon';
 import { ArrowIcon } from '@/app/_components/ui/iconPath';
-import { postPreJobs } from '../_services/mainService';
+import { fetchAllPreJobs, postPreJobs } from '../_services/mainService';
+import useCustomQuery from '@/app/_hooks/useCustomQuery';
 
 interface InterestedJobProps {
   onClose: () => void;
@@ -29,13 +29,18 @@ interface CategoryData extends Array<Category> {}
 
 export default function InterestedJob({ onClose }: InterestedJobProps) {
   const { handleSubmit } = useForm({});
+  const { data: jobData } = useCustomQuery(['pre-jobs'], () => fetchAllPreJobs());
 
-  const [selectJobCategory, setSelectJobCategory] = useState<string>('직군 전체');
+  const [selectJobCategory, setSelectJobCategory] = useState('직군 전체');
   const [preJob, setPreJob] = useState<number[]>([]);
 
-  const categoryData: CategoryData = jobData;
+  const categoryData: CategoryData = useMemo(() => {
+    return jobData && jobData.success ? jobData.data : [];
+  }, [jobData]);
 
-  const allJobs = useMemo(() => categoryData.flatMap((category) => category.jobs), [categoryData]); // 모든 직업을 하나의 배열로 합침(직군 전체), flat이 평탄화 하는 용도
+  const allJobs = useMemo(() => {
+    return categoryData.flatMap((category) => category.jobs);
+  }, [categoryData]); // 모든 직업을 하나의 배열로 합침(직군 전체), flat이 평탄화 하는 용도
 
   // 현재 선택된 카테고리에 따라 표시할 직업 목록을 계산
   const jobs = useMemo(() => {
@@ -79,8 +84,8 @@ export default function InterestedJob({ onClose }: InterestedJobProps) {
           <p>관심 직군을 선택해주세요</p>
         </div>
 
-        <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col w-full">
-          <div className="flex justify-center w-full max-h-64 py-2">
+        <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col w-full flex-grow">
+          <div className="flex justify-center w-full h-full max-h-64 py-2">
             {/* 직군 카테고리 */}
             <div className="flex flex-col w-full max-w-60 overflow-scroll hide-scrollbar">
               {/* 주어진 직업 객체의 열거 가능한 속성 이름들(카테고리)을 배열로 반환 */}
@@ -91,7 +96,7 @@ export default function InterestedJob({ onClose }: InterestedJobProps) {
                   selectJobCategory === '직군 전체' ? 'bg-primary-0' : 'text-gray-0'
                 }`}
               >
-                <p className="w-full text-center">전체</p>
+                <p className="w-full text-center">직군 전체</p>
                 <Icons className="bg-white-0 border rounded-full rotate-180" name={ArrowIcon} />
               </button>
               {categoryData.map((category) => (
@@ -111,7 +116,7 @@ export default function InterestedJob({ onClose }: InterestedJobProps) {
             {/* 중앙선 */}
             <div className="w-[1px] h-full bg-gray-5 mx-3 sm:mx-6 md:mx-8" />
             {/* 직군 상세 */}
-            <div className="flex items-center flex-col w-full max-w-52 pr-0 sm:pr-4 overflow-scroll hide-scrollbar">
+            <div className="flex items-center flex-col w-full max-w-52 h-44 pr-0 sm:pr-4 overflow-scroll hide-scrollbar">
               {jobs.map((job) => (
                 <button
                   type="button"
@@ -126,6 +131,7 @@ export default function InterestedJob({ onClose }: InterestedJobProps) {
                     {job.jobName}
                   </p>
 
+                  {/* 동그라미 */}
                   {preJob.includes(job.jobId) ? (
                     <div className="flex-shrink-0 flex-center w-5 h-5 border-2 ml-2 border-primary-1 rounded-full">
                       <div className="w-[10px] h-[10px] min-w-[10px] max-h-[10px] bg-primary-1 rounded-full" />
