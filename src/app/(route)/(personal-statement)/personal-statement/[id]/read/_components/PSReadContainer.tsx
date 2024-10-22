7907babab2482ecc87724534dec7d891b21f71bc
jiyeon <bjyy0201@gmail.com>
2024-10-22 22:56:19 +0900

    ✨ feat: [자소서]: 자소서 조회 api 셋팅
    - 목록에서 상세 페이지 이동시 해당 내용 가져오기 위한 api 연결
    
    KAN-145

--- src/app/(route)/(personal-statement)/personal-statement/[id]/read/_components/PSReadContainer.tsx
+++ src/app/(route)/(personal-statement)/personal-statement/[id]/read/_components/PSReadContainer.tsx
@@ -1,18 +1,33 @@
 'use client';
 
 import { useEffect, useState } from 'react';
-import { useRouter } from 'next/navigation';
+import { useRouter, useParams } from 'next/navigation';
 import { usePDF, Margin } from 'react-to-pdf';
-import psReadTest from './psReadTest.json';
-import BackButton from '../../../../../../_components/BackButton';
+import Image from 'next/image';
 import Modal, { initailModalState } from '../../../../../../_components/Modal';
 import { PSFormData } from '../../create/_types/psCreate';
 import PSFooter from '../../../../_components/PSFooter';
 import PSHeader from '../../../../_components/PSHeader';
 import PSReadContent from './PSReadContent';
+import { fetchPS } from '@/app/(route)/(personal-statement)/_services/psServices';
+import useCustomQuery from '@/app/_hooks/useCustomQuery';
 
 export default function PSReadContainer() {
+  // const params = useParams();
   const router = useRouter();
+  const params = useParams();
+
+  const getPostId = (param: string | string[]): string => {
+    if (Array.isArray(param)) {
+      return param[1]; // 배열인 경우 첫 번째 요소를 사용
+    }
+    return param;
+  };
+
+  const postId = decodeURI(getPostId(params.id));
+
+  const { data: psData } = useCustomQuery(['ps', postId], () => fetchPS(postId));
+
   const [psState, setPsState] = useState<PSFormData>({
     title: '',
     position: '',
@@ -23,8 +38,8 @@ export default function PSReadContainer() {
   const [modalState, setModalState] = useState(initailModalState);
 
   useEffect(() => {
-    setPsState(psReadTest[0]);
-  }, []);
+    setPsState(psData?.data);
+  }, [psData]);
 
   // 삭제 클릭
   const handleDeleteClick = () => {
@@ -56,25 +71,33 @@ export default function PSReadContainer() {
 
   return (
     <>
-      <BackButton className="self-start pb-4" />
-      <PSHeader
-        title={psState.title}
-        mode="read"
-        onButtonClick={() => {
-          router.push('/personal-statement/1/editing');
-        }}
-        handleDeleteClick={handleDeleteClick}
-      />
+      {psState ? (
+        <div className="flex-col  w-full h-full">
+          <PSHeader
+            title={psState.title}
+            mode="read"
+            onButtonClick={() => {
+              router.push('/personal-statement/1/editing');
+            }}
+            handleDeleteClick={handleDeleteClick}
+          />
 
-      <div ref={targetRef} className="self-start w-full mt-8 sm:mt-20">
-        <p className="w-full font-semibold text-[28px] pb-5 mb-9 border-b-2 border-5">자기소개서</p>
+          <div ref={targetRef} className="self-start w-full mt-8 sm:mt-20">
+            <p className="w-full font-semibold text-[28px] pb-5 mb-9 border-b-2 border-5">자기소개서</p>
 
-        <div className="flex flex-col gap-11">
-          <PSReadContent label="지원 직무" content={psState.position} />
-          <PSReadContent label="지원 사유" content={psState.reason} />
-          <PSReadContent label="자기소개" content={psState.content} />
+            <div className="flex flex-col gap-11">
+              <PSReadContent label="지원 직무" content={psState.position} />
+              <PSReadContent label="지원 사유" content={psState.reason} />
+              <PSReadContent label="자기소개" content={psState.content} />
+            </div>
+          </div>
         </div>
-      </div>
+      ) : (
+        <div className="flex flex-col gap-3 mt-44">
+          <Image className="animate-bounce" src="/mascot.png" alt="마스코트" width={204} height={193} />
+          <p className="text-gray-0">불러오는 중 입니다</p>
+        </div>
+      )}
       <PSFooter
         showPDF
         showBack
