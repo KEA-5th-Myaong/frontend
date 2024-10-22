4ba152f0bacd653ec3da492f51145d08b190847c
kimgorok <kjyse2@naver.com>
2024-10-22 22:19:09 +0900

    ✨feat: [모의면접] 기업 목록 검색 기능 추가
    - fetchCompaniesSearch를 QueryString으로 변경 후 검색 기능 추가
    
    KAN-148

--- src/app/(route)/(interview)/interview/[id]/select/page.tsx
+++ src/app/(route)/(interview)/interview/[id]/select/page.tsx
@@ -7,7 +7,7 @@ import Icons from '../../../../../_components/ui/Icon';
 import { SearchIcon } from '../../../../../_components/ui/iconPath';
 import ProgressBar from '../../../_components/ProgressBar';
 import useCustomQuery from '@/app/_hooks/useCustomQuery';
-import { fetchCompanies } from '../../../_services/interviewService';
+import { fetchCompanies, fetchCompaniesSearch } from '../../../_services/interviewService';
 
 export default function InterviewSelect() {
   const router = useRouter();
@@ -16,9 +16,30 @@ export default function InterviewSelect() {
   const { data } = useCustomQuery(['corps'], () => fetchCompanies());
 
   const [corpList, setCorpList] = useState([]);
+  const [searchCorp, setSearchCorp] = useState('');
+
+  // 기업 목록 불러와서 저장
   useEffect(() => {
     setCorpList(data?.data);
   }, [data?.data]);
+
+  // 기업 목록 검색해서 저장
+  const handleSearch = async () => {
+    if (searchCorp.trim()) {
+      try {
+        const result = await fetchCompaniesSearch(searchCorp);
+        setCorpList(result.data);
+      } catch (error) {
+        console.error('검색 중 오류 발생:', error);
+      }
+    }
+  };
+
+  const handleKeyDown = (e: { key: string }) => {
+    if (e.key === 'Enter') {
+      handleSearch();
+    }
+  };
 
   return (
     <section className="interview-container">
@@ -27,8 +48,14 @@ export default function InterviewSelect() {
 
       <div className="flex gap-4 self-stretch pt-6 min-w-[318px]">
         <div className="flex self-stretch items-center px-5 py:1 md:py-4 gap-5 rounded-[3rem] border border-primary-1 w-full sm:w-[320px] lg:w-full bg-white-0">
-          <Icons name={SearchIcon} />
-          <input className="w-full focus:outline-none" placeholder="기업 이름을 검색하거나 선택하세요" />
+          <Icons name={SearchIcon} onClick={handleSearch} />
+          <input
+            className="w-full focus:outline-none"
+            placeholder="기업 이름을 검색하거나 선택하세요"
+            value={searchCorp}
+            onChange={(e) => setSearchCorp(e.target.value)}
+            onKeyDown={handleKeyDown}
+          />
         </div>
         <motion.button
           type="button"
