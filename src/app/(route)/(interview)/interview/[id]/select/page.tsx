0833c585dede45bd0f2c10ee18d7503018f9a9a3
kimgorok <kjyse2@naver.com>
2024-10-23 10:01:57 +0900

    ✨feat: [모의면접] 기업 검색 api 연결
    - 기업 검색 api 연결
    - api 주소 수정
    
    KAN-148

--- src/app/(route)/(interview)/interview/[id]/select/page.tsx
+++ src/app/(route)/(interview)/interview/[id]/select/page.tsx
@@ -13,7 +13,7 @@ export default function InterviewSelect() {
   const router = useRouter();
   const id = useParams();
 
-  const { data } = useCustomQuery(['corps'], () => fetchCompanies());
+  const { data, isLoading } = useCustomQuery(['corps'], () => fetchCompanies());
 
   const [corpList, setCorpList] = useState([]);
   const [searchCorp, setSearchCorp] = useState('');
@@ -46,9 +46,9 @@ export default function InterviewSelect() {
       <ProgressBar progress={3} />
       <p className="font-semibold self-start">모의 면접</p>
 
-      <div className="flex gap-4 self-stretch pt-6 min-w-[318px]">
-        <div className="flex self-stretch items-center px-5 py:1 md:py-4 gap-5 rounded-[3rem] border border-primary-1 w-full sm:w-[320px] lg:w-full bg-white-0">
-          <Icons name={SearchIcon} onClick={handleSearch} />
+      <div className="flex gap-4 self-stretch pt-6 min-w-[318px] w-full">
+        <div className="flex self-stretch items-center px-5 py:1 md:py-4 gap-5 rounded-[3rem] border border-primary-1 w-full bg-white-0">
+          <Icons name={SearchIcon} />
           <input
             className="w-full focus:outline-none"
             placeholder="기업 이름을 검색하거나 선택하세요"
@@ -59,26 +59,30 @@ export default function InterviewSelect() {
         </div>
         <motion.button
           type="button"
+          onClick={handleSearch}
           layoutId="select"
           className="px-4 md:px-10 py-2 md:py-4 rounded-[28px] primary-1-btn"
         >
-          선택
+          검색
         </motion.button>
       </div>
 
       <div className="flex flex-wrap mx-auto pt-5 w-full min-w-[263px] max-w-[440px]">
-        {corpList?.map((corp) => (
-          <motion.button
-            type="button"
-            layoutId={`corp-${corp}`}
-            onClick={() => {
-              router.push(`/interview/${id.id}/${corp.companyName}/personal-statement`);
-            }}
-            className="corp-block"
-          >
-            {corp.companyName}
-          </motion.button>
-        ))}
+        {isLoading
+          ? Array.from({ length: 28 }).map(() => (
+              <div className="h-7 py-1.5 px-1 sm:px-4 md:px-6 lg:px-8 my-2  rounded-lg w-1/3 bg-gray-4 animate-pulse" />
+            ))
+          : corpList?.map((corp) => (
+              <motion.button
+                type="button"
+                onClick={() => {
+                  router.push(`/interview/${id.id}/${corp.companyName}/personal-statement`);
+                }}
+                className="corp-block"
+              >
+                {corp.companyName}
+              </motion.button>
+            ))}
       </div>
     </section>
   );
