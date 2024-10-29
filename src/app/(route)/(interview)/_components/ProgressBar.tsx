fb9d7f9a7448d580fe9426fdd90c0e9b581474a1
kimgorok <kjyse2@naver.com>
2024-10-29 13:08:00 +0900

    💡comment: [모의면접] ProgressBar 주석 처리
    - ProgressBar.tsx 전문 주석 처리
    
    KAN-170

--- src/app/(route)/(interview)/_components/ProgressBar.tsx
+++ src/app/(route)/(interview)/_components/ProgressBar.tsx
@@ -1,20 +1,20 @@
-// 사용되지 않는 프로그레스바
+// // 사용되지 않는 프로그레스바
 
-export default function ProgressBar({ progress }: { progress: number }) {
-  return (
-    <div
-      className="absolute -top-32 md:-top-2 flex flex-col gap-2 w-full 
-     md:w-[calc(100%-64px)] lg:w-[calc(100%-80px)] xl:w-[calc(100%-96px)]"
-    >
-      <div className="flex justify-between w-full text-sm">
-        <p>기업 선택</p>
-        <p>자소서 선택</p>
-        <p>질문 선택</p>
-        <p>AI 모의 면접 시작</p>
-      </div>
-      <div className="bg-gray-5 h-2 w-full rounded-[10px]">
-        <div style={{ width: `${progress}%` }} className="bg-primary-1 h-2 rounded-[10px]" />
-      </div>
-    </div>
-  );
-}
+// export default function ProgressBar({ progress }: { progress: number }) {
+//   return (
+//     <div
+//       className="absolute -top-32 md:-top-2 flex flex-col gap-2 w-full
+//      md:w-[calc(100%-64px)] lg:w-[calc(100%-80px)] xl:w-[calc(100%-96px)]"
+//     >
+//       <div className="flex justify-between w-full text-sm">
+//         <p>기업 선택</p>
+//         <p>자소서 선택</p>
+//         <p>질문 선택</p>
+//         <p>AI 모의 면접 시작</p>
+//       </div>
+//       <div className="bg-gray-5 h-2 w-full rounded-[10px]">
+//         <div style={{ width: `${progress}%` }} className="bg-primary-1 h-2 rounded-[10px]" />
+//       </div>
+//     </div>
+//   );
+// }
