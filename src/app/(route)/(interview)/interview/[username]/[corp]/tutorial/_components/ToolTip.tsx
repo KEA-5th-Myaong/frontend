2aba72fde105081cdcdc9cdadeb33d897bf5d7b9
kimgorok <kjyse2@naver.com>
2024-11-02 21:37:57 +0900

    ✨feat: [모의면접] 튜토리얼 페이지 레이아웃 제작
    - assets 이미지 추가
    - ToolTip.tsx 컴포넌트 작성
    - 재생, 새로고침 아이콘 추가
    - 튜토리얼 페이지 state 적용
    
    KAN-170

---
+++ src/app/(route)/(interview)/interview/[username]/[corp]/tutorial/_components/ToolTip.tsx
@@ -0,0 +1,16 @@
+import Image from 'next/image';
+
+export default function Tooltip({ msg1, msg2, className }: { msg1: string; msg2: string; className?: string }) {
+  return (
+    <div
+      className={`absolute flex flex-col items-center pt-2 pb-4 px-6 bg-white-0 ${className || 'left-0 bottom-14'}  
+                    border border-[#D8E6F1] rounded-[28px] text-center font-semibold text-[#8A8A8A] text-[11px]`}
+    >
+      <div className="w-12 h-12 flex-shrink-0">
+        <Image width={48} height={48} src="/mascot.png" alt="이미지" className="w-full h-full object-contain" />
+      </div>
+      <p>{msg1}</p>
+      <p>{msg2}</p>
+    </div>
+  );
+}
--- src/app/(route)/(interview)/interview/[username]/[corp]/tutorial/page.tsx
+++ src/app/(route)/(interview)/interview/[username]/[corp]/tutorial/page.tsx
@@ -1,14 +1,28 @@
 'use client';
 
 import { useState } from 'react';
+import Image from 'next/image';
 import Icons from '@/app/_components/ui/Icon';
-import { ArrowIcon, PlusIcon, TriangleIcon, XIcon } from '@/app/_components/ui/iconPath';
+import { ArrowIcon, PlayIcon, PlusIcon, RefreshIcon, TriangleIcon, XIcon } from '@/app/_components/ui/iconPath';
+import Tooltip from './_components/ToolTip';
 
 export default function Tutorial() {
   const [num, setNum] = useState(1);
   return (
-    <div className="fixed inset-0 flex-center bg-black-3 bg-opacity-25 z-50">
-      {/* 왼쪽에 그거 */}
+    <div
+      role="button"
+      onKeyDown={(e) => {
+        if (e.key === 'Enter' || e.key === ' ') {
+          setNum((prev) => prev + 1);
+        }
+      }}
+      tabIndex={-1}
+      onClick={() => {
+        setNum((prev) => prev + 1);
+      }}
+      className="fixed inset-0 flex-center bg-black-3 bg-opacity-25 z-50"
+    >
+      {/* 면접 기록 */}
       <div className="hidden md:block mt-24 mr-4 md:max-w-[253px] md:w-full self-stretch max-h-fit border-2 border-gray-5 pt-[29px] pb-[22px] px-5 md:px-2 lg:px-5 rounded-2xl font-semibold">
         <div className="flex justify-between w-full">
           <p className="pl-[13px] mb-0 md:mb-5">면접 기록</p>
@@ -27,8 +41,8 @@ export default function Tutorial() {
           면접 생성
         </div>
       </div>
-      {/* 여까지 */}
 
+      {/* 본문 */}
       <section className="relative bg-[#CBCBCE] flex flex-col pb-8 pt-6 sm:pt-3.5 interview-container px-4 min-w-[360px] z-50">
         <p className="font-semibold self-start">모의 면접</p>
         <div className="flex flex-col self-stretch pt-2 w-full pb-8">
@@ -38,14 +52,71 @@ export default function Tutorial() {
             <div className="relative w-full max-w-64 py-4 px-4 sm:px-5 bg-gray-4 font-bold rounded-[28px] z-30 bg-opacity-25 ">
               위볼린
             </div>
-            <div className="py-4 px-7 lg:px-9 rounded-[28px] primary-1-btn bg-opacity-25 ">포스트 작성</div>
+            <div className={`relative py-4 px-7 lg:px-9 rounded-[28px] primary-1-btn ${num !== 3 && 'bg-opacity-25'}`}>
+              {num === 3 && <Tooltip msg1="면접 내용을 바탕으로" msg2="포스트 작성이 가능해요" />}
+              포스트 작성
+            </div>
           </div>
         </div>
 
         {/* 여기부터 채팅창 */}
         <div className="flex flex-col w-full relative min-w-[360px] max-w-[735px] px-4 border-t border-gray-2">
-          <div className="fixed left-12 bottom-12 z-30">
-            <div className="py-4  px-6 rounded-[28px] primary-1-btn mt-2 md:block hidden bg-opacity-25">
+          {/* 비디오 튜토리얼 */}
+          {(num === 5 || num === 6) && (
+            <div className="fixed  md:left-12 md:bottom-12 min-w-[360px] z-10">
+              <div className="relative bg-white-0 px-10 rounded-xl max-w-[460px] border border-gray-5 pt-5 pb-11">
+                <div className="w-full flex justify-between pb-[14px] border-b border-gray-5">
+                  <p className="font-semibold">면접 연습하기</p>
+                  <div className="flex items-center border border-primary-3 py-1 px-4 rounded-[28px]">
+                    <Icons name={PlayIcon} />
+                    녹화 시작
+                  </div>
+                </div>
+
+                <Image width={360} height={208} src="/assets/videoImage.png" alt="이미지" />
+
+                <div className="w-full flex justify-between mt-5 ">
+                  <p className="font-semibold">AI 표정 분석 결과</p>
+                  <div className="flex items-center border border-primary-3 py-1 px-4 rounded-[28px]">
+                    <Icons name={RefreshIcon} />
+                    다시 보기
+                  </div>
+                </div>
+
+                <div className="flex gap-2">
+                  <div className="w-12 h-12 flex-shrink-0">
+                    <Image className="w-full h-full" width={33} height={33} src="/mascot.png" alt="이미지" />
+                  </div>
+                  <div className="mt-3 font-medium text-[11px] bg-[#F5F5F5] rounded-[20px] py-4 px-8">
+                    <p>
+                      면접 초반의 간단한 질문에 대해 미소와 안정적인 표정을 유지하며 긍정적인 태도가 잘 나타났습니다.
+                      이는 지원자의 자신감과 호감도를 높이는 데 도움이 됩니다.
+                    </p>
+                    <p className="hidden md:block">
+                      어려웠던 경험을 설명해 주세요 질문에 대한 답변 도중, 순간적으로 긴장도가 높아지며 눈 깜박임 빈도와
+                      입술을 다무는 행동이 관찰되었습니다. 이와 같은 표정은 불안감이나 스트레스를 드러낼 수 있습니다.
+                    </p>
+                  </div>
+                </div>
+              </div>
+              {num === 5 && (
+                <Tooltip
+                  msg1="면접 영상을 녹화해"
+                  msg2="다시 볼 수 있어요"
+                  className="left-12 bottom-[480px] md:bottom-[540px]"
+                />
+              )}
+              {num === 6 && (
+                <Tooltip msg1="AI 표정 분석 결과도" msg2="확인 가능해요!" className="right-8 bottom-7 md:bottom-20" />
+              )}
+            </div>
+          )}
+
+          <div className="fixed left-12 bottom-12">
+            <div
+              className={`py-4  px-6 rounded-[28px] primary-1-btn mt-2 md:block hidden ${num !== 4 && 'bg-opacity-25'}`}
+            >
+              {num === 4 && <Tooltip msg1="입력한 답변을 말해보며" msg2="AI로 표정 분석을 받을 수 있어요" />}
               AI 표정 분석
             </div>
           </div>
@@ -74,13 +145,17 @@ export default function Tutorial() {
 
               {/* 질문 버튼들 */}
               <div className="flex items-center gap-4 pt-4">
-                <div className="chat-msg-btn bg-opacity-25 flex md:hidden">
+                <div className={`chat-msg-btn flex md:hidden relative ${num !== 4 && 'bg-opacity-25'}`}>
+                  {num === 4 && <Tooltip msg1="입력한 답변을 말해보며" msg2="AI로 표정 분석을 받을 수 있어요" />}
                   AI 표정 분석 <Icons className="rotate-180 border rounded-full" name={ArrowIcon} />
                 </div>
-                <div className="chat-msg-btn bg-opacity-25 ">
+                <div className={`chat-msg-btn ${num !== 1 && 'bg-opacity-25'} relative`}>
+                  {num === 1 && <Tooltip msg1="답변에 대한" msg2="꼬리 질문을 받을 수 있어요" />}
                   꼬리 질문 받기 <Icons className="rotate-180 border rounded-full" name={ArrowIcon} />
                 </div>
-                <div className="chat-msg-btn bg-opacity-25 ">
+
+                <div className={`chat-msg-btn relative ${num !== 2 && 'bg-opacity-25'}`}>
+                  {num === 2 && <Tooltip msg1="새로운 질문 생성도" msg2="가능해요" />}
                   새 질문 생성 <Icons className="rotate-180 border rounded-full" name={ArrowIcon} />
                 </div>
               </div>
