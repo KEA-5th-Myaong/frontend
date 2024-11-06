6a5af933d9a4721f0b6c50fb70ccd42e6a440e51
kimgorok <kjyse2@naver.com>
2024-11-06 19:53:44 +0900

    💄design: [관리자] 관리자 메인 페이지 아이콘 추가
    - 관리자 메인 페이지에 들어가는 각종 아이콘 추가
    - globals.css 정리
    
    KAN-185

--- src/app/(admin-route)/(admin)/admin/page.tsx
+++ src/app/(admin-route)/(admin)/admin/page.tsx
@@ -1,21 +1,30 @@
+'use client';
+
+import Link from 'next/link';
+import Image from 'next/image';
+
 export default function AdminMain() {
+  const handleLogoutClick = () => {};
   return (
     <section className="flex-center min-h-screen bg-[#F5F5F5] pt-28 px-5">
-      <div className="grid grid-cols-1 sm:grid-cols-2 gap-7 font-semibold min-w-[360px] w-full max-w-[768px] pb-14">
+      <div className="grid grid-cols-1 sm:grid-cols-2 gap-7 font-semibold min-w-[360px] w-full max-w-[768px] pb-40">
         {/* 관리자 */}
         <div className="flex-center flex-col bg-black-3 h-[286px] pt-10 pb-9 text-white-0 font-semibold w-full max-w-h-96 border-gray-3">
-          <div className="w-[100px] min-h-[100px] bg-white-0 rounded-full" />
+          <Image width={120} height={120} src="./assets/admin/main-admin.svg" alt="" />
           <p className="mt-4">관리자</p>
           <p className="mt-3 font-medium">yeonilil@naver.com</p>
-          <p className="mt-5">로그아웃</p>
+          <button type="button" onClick={handleLogoutClick} className="mt-5 flex gap-1.5 items-center">
+            <Image width={20} height={20} src="./assets/admin/main-logout.svg" alt="" />
+            로그아웃
+          </button>
         </div>
 
         {/* 알림 */}
-        <div className="h-[286px] pl-10 pr-6 pt-9 border w-full max-w-h-96 border-gray-3 font-semibold bg-white-0">
-          <div className="pb-2.5 border-b border-gray-0">
-            <p>알림</p>
+        <div className="pl-10 pr-6 pt-9 w-full h-[286px] max-w-h-96 border border-gray-3 font-semibold bg-white-0">
+          <div className="flex gap-3 pb-2.5 border-b border-gray-0">
+            <Image width={20} height={20} src="./assets/admin/main-alert.svg" alt="" />
+            알림
           </div>
-
           <div className="mt-6 w-full flex justify-between text-xs">
             <p>답변 대기 문의</p>
             <p className="text-red-0 pr-14">2건</p>
@@ -23,22 +32,53 @@ export default function AdminMain() {
         </div>
 
         {/* 콘텐츠 관리 */}
-        <div className="py-8 pl-6 pr-[14px] bg-white-0 border w-full max-w-h-96 border-gray-3">콘텐츠 관리</div>
-
-        {/* 콘텐츠 관리? */}
-        <div className="py-8 pl-6 pr-[14px] bg-white-0 border w-full max-w-h-96 border-gray-3">콘텐츠 관리?</div>
-
-        {/* 회원관리 */}
-        <div className="py-8 pl-6 pr-[14px] bg-white-0 border w-full max-w-h-96 border-gray-3">회원 관리</div>
-
+        <Link href="/" className="manage-page-block">
+          <div className="manage-icon">
+            <Image width={20} height={20} src="./assets/admin/main-contents.svg" alt="" />
+            콘텐츠 관리
+          </div>
+          <Image width={40} height={40} src="./assets/arrow-right.svg" alt=">" />
+        </Link>
+        {/* 기업 관리 */}
+        <Link href="/" className="manage-page-block">
+          <div className="manage-icon">
+            <Image width={20} height={20} src="./assets/admin/main-corp.svg" alt="" />
+            기업 관리
+          </div>
+          <Image width={40} height={40} src="./assets/arrow-right.svg" alt=">" />
+        </Link>
+        {/* 회원 관리 */}
+        <Link href="/" className="manage-page-block">
+          <div className="manage-icon">
+            <Image width={20} height={20} src="./assets/admin/main-member.svg" alt="" />
+            회원 관리
+          </div>
+          <Image width={40} height={40} src="./assets/arrow-right.svg" alt=">" />
+        </Link>
         {/* 회원 카테고리 관리 */}
-        <div className="py-8 pl-6 pr-[14px] bg-white-0 border w-full max-w-h-96 border-gray-3">회원 카테고리 관리</div>
-
+        <Link href="/" className="manage-page-block">
+          <div className="manage-icon">
+            <Image width={20} height={20} src="./assets/admin/main-membercategory.svg" alt="" />
+            회원 카테고리 관리
+          </div>
+          <Image width={40} height={40} src="./assets/arrow-right.svg" alt=">" />
+        </Link>
         {/* 문의 관리 */}
-        <div className="py-8 pl-6 pr-[14px] bg-white-0 border w-full max-w-h-96 border-gray-3">문의 관리</div>
-
+        <Link href="/" className="manage-page-block">
+          <div className="manage-icon">
+            <Image width={20} height={20} src="./assets/admin/main-faq.svg" alt="" />
+            문의 관리
+          </div>
+          <Image width={40} height={40} src="./assets/arrow-right.svg" alt=">" />
+        </Link>
         {/* 공지 관리 */}
-        <div className="py-8 pl-6 pr-[14px] bg-white-0 border w-full max-w-h-96 border-gray-3">공지 관리</div>
+        <Link href="/" className="manage-page-block">
+          <div className="manage-icon">
+            <Image width={20} height={20} src="./assets/admin/main-notice.svg" alt="" />
+            공지 관리
+          </div>
+          <Image width={40} height={40} src="./assets/arrow-right.svg" alt=">" />
+        </Link>
       </div>
     </section>
   );
