a88dbc2277559b81e9d468f9b47d2f8e30ba35b4
kimgorok <kjyse2@naver.com>
2024-11-07 15:47:27 +0900

    🎉init: [관리자] 관리자 사이드바 추가
    - AdminSideBar.tsx 제작
    - 관리자 페이지 layout.tsx 수정
    - 관리자 메인 이미지 경로 수정
    
    KAN-185

--- src/app/(admin-route)/layout.tsx
+++ src/app/(admin-route)/layout.tsx
@@ -11,10 +11,10 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <body>
-        <Link href="/main" className="absolute ml-16 pt-9 bg-[#F5F5F5]">
+        <Link href="/main" className="absolute ml-16 pt-9">
           <Image src="/assets/adminLogo.svg" alt="로고" width={103} height={66} />
         </Link>
-        {children}
+        <section className="bg-[#F5F5F5] min-h-screen pt-28 px-5">{children}</section>
       </body>
     </html>
   );
