78701dad477a96ca76f5e69f3b8a6ba8d764030a
yeonilil <gwagseoyeon@gmail.com>
2024-11-07 11:52:33 +0900

    🐛 fix: 항목 편집 스크롤해도 고정

--- src/app/(route)/(portfolio)/portfolio/[portfolioId]/write/_components/LoadPSModal.tsx
+++ src/app/(route)/(portfolio)/portfolio/[portfolioId]/write/_components/LoadPSModal.tsx
@@ -1,8 +1,14 @@
 'use client';
 
 import { motion } from 'framer-motion';
+import { useState } from 'react';
 import { modalMotion } from '@/app/_components/Modal';
 import Overlay from '@/app/_components/Overlay';
+import { fetchPSList } from '@/app/(route)/(personal-statement)/_services/psServices';
+import useCustomQuery from '@/app/_hooks/useCustomQuery';
+import { PSListBoxProps } from '@/app/(route)/(personal-statement)/personal-statement/[id]/list/_types/psList';
+import Image from 'next/image';
+import Link from 'next/link';
 
 interface LoadPSModalProps {
   onOverlayClick?: () => void;
@@ -14,6 +20,11 @@ export default function LoadPSModal({ onOverlayClick }: LoadPSModalProps) {
       onOverlayClick();
     }
   };
+
+  // 자기소개서 목록 불러오기
+  const [personalStatement, setPersonalStatement] = useState<PSListBoxProps[]>([]);
+  const { data: psListData } = useCustomQuery(['psList'], () => fetchPSList());
+
   return (
     <Overlay onClick={handleOverlayClick}>
       <motion.div
@@ -22,80 +33,44 @@ export default function LoadPSModal({ onOverlayClick }: LoadPSModalProps) {
         className="flex m-4 min-w-[300px]  max-w-[1000px] h-[500px] hide-scrollbar overflow-y-scroll w-full pb-6 px-[65px] pt-[58px] flex-col items-start gap-6 rounded-2xl bg-white-0 shadow-md"
       >
         <h1 className="pre-2xl-semibold">자기소개서 불러오기</h1>
-        <section className="flex justify-between px-6 pb-4 pt-6 sm:px-8 sm:pb-4 sm:pt-10 border border-gray-2 rounded-lg bg-white-0 cursor-pointer">
-          <div>
-            <div className="flex items-center gap-4">
-              <p className="font-semibold text-lg sm:text-xl line-clamp-2">자기소개서 제목</p>
-              <div className="hidden sm:block bg-primary-1 rounded-md px-5 py-1 text-[11px] text-white-0">
-                지원직무 : 프론트엔드 개발자
-              </div>
+        {personalStatement.length === 0 ? (
+          <>
+            <div className="flex flex-col gap-3 mt-10 ml-[350px]">
+              <Image className="animate-bounce" src="/mascot.png" alt="마스코트" width={204} height={193} />
+              <p className="text-gray-0">자기소개서를 작성해보세요</p>
             </div>
+            {/* <Link href={`personal-statement/{id}/create`}> <- 작성 화면으로 이동 추가*/}
+            <button
+              type="button"
+              className=" ml-[370px] flex items-center h-[50px] py-[13px] gap-2.5 md:py-[19px] px-5 md:px-7 rounded-[30px] primary-1-btn hover-animation"
+            >
+              작성하러 가기
+            </button>
+            {/* </Link> */}
+          </>
+        ) : (
+          personalStatement.map((ps) => (
+            <section className="flex justify-between px-6 pb-4 pt-6 sm:px-8 sm:pb-4 sm:pt-10 border border-gray-2 rounded-lg bg-white-0 cursor-pointer">
+              <div>
+                <div className="flex items-center gap-4">
+                  <p className="font-semibold text-lg sm:text-xl line-clamp-2">{ps.title}</p>
+                  <div className="hidden sm:block bg-primary-1 rounded-md px-5 py-1 text-[11px] text-white-0">
+                    지원직무 : {ps.position}
+                  </div>
+                </div>
 
-            <p className="mr-5 text-gray-0 text-sm line-clamp-5 mt-3">
-              안녕하세요. 저는 사용자 경험을 중요시하는 프론트엔드 개발자 곽서연입니다. 웹 개발에 대한 열정은 고등학교
-              시절 아두이노 프로젝트를 진행하면서 생겼고, 이후 인공지능학과에 진학하면서 더 넓은 범위의 기술을 배우게
-              되었습니다. 학부 3학년 때 웹 개발의 매력에 빠져, React와 Next.js 등의 최신 기술을 익히며, 다양한
-              프로젝트를 통해 실력을 쌓았습니다.
-            </p>
-            <p className="text-gray-0 text-xs sm:text-sm pt-4">2024.03.02 등록</p>
-          </div>
-          <button
-            type="button"
-            className="mt-9 flex items-center h-[50px] py-[13px] gap-2.5 md:py-[19px] px-5 md:px-7 rounded-[30px] primary-1-btn hover-animation"
-          >
-            불러오기
-          </button>
-        </section>
-
-        <section className="flex justify-between px-6 pb-4 pt-6 sm:px-8 sm:pb-4 sm:pt-10 border border-gray-2 rounded-lg bg-white-0 cursor-pointer">
-          <div>
-            <div className="flex items-center gap-4">
-              <p className="font-semibold text-lg sm:text-xl line-clamp-2">자기소개서 제목</p>
-              <div className="hidden sm:block bg-primary-1 rounded-md px-5 py-1 text-[11px] text-white-0">
-                지원직무 : 프론트엔드 개발자
+                <p className="mr-5 text-gray-0 text-sm line-clamp-5 mt-3">{ps.content}</p>
+                <p className="text-gray-0 text-xs sm:text-sm pt-4">{ps.timestamp} 등록</p>
               </div>
-            </div>
-
-            <p className="mr-5 text-gray-0 text-sm line-clamp-5 mt-3">
-              안녕하세요. 저는 사용자 경험을 중요시하는 프론트엔드 개발자 곽서연입니다. 웹 개발에 대한 열정은 고등학교
-              시절 아두이노 프로젝트를 진행하면서 생겼고, 이후 인공지능학과에 진학하면서 더 넓은 범위의 기술을 배우게
-              되었습니다. 학부 3학년 때 웹 개발의 매력에 빠져, React와 Next.js 등의 최신 기술을 익히며, 다양한
-              프로젝트를 통해 실력을 쌓았습니다.
-            </p>
-            <p className="text-gray-0 text-xs sm:text-sm pt-4">2024.03.02 등록</p>
-          </div>
-          <button
-            type="button"
-            className="mt-9 flex items-center h-[50px] py-[13px] gap-2.5 md:py-[19px] px-5 md:px-7 rounded-[30px] primary-1-btn hover-animation"
-          >
-            불러오기
-          </button>
-        </section>
-
-        <section className="flex justify-between px-6 pb-4 pt-6 sm:px-8 sm:pb-4 sm:pt-10 border border-gray-2 rounded-lg bg-white-0 cursor-pointer">
-          <div>
-            <div className="flex items-center gap-4">
-              <p className="font-semibold text-lg sm:text-xl line-clamp-2">자기소개서 제목</p>
-              <div className="hidden sm:block bg-primary-1 rounded-md px-5 py-1 text-[11px] text-white-0">
-                지원직무 : 프론트엔드 개발자
-              </div>
-            </div>
-
-            <p className="mr-5 text-gray-0 text-sm line-clamp-5 mt-3">
-              안녕하세요. 저는 사용자 경험을 중요시하는 프론트엔드 개발자 곽서연입니다. 웹 개발에 대한 열정은 고등학교
-              시절 아두이노 프로젝트를 진행하면서 생겼고, 이후 인공지능학과에 진학하면서 더 넓은 범위의 기술을 배우게
-              되었습니다. 학부 3학년 때 웹 개발의 매력에 빠져, React와 Next.js 등의 최신 기술을 익히며, 다양한
-              프로젝트를 통해 실력을 쌓았습니다.
-            </p>
-            <p className="text-gray-0 text-xs sm:text-sm pt-4">2024.03.02 등록</p>
-          </div>
-          <button
-            type="button"
-            className="mt-9 flex items-center h-[50px] py-[13px] gap-2.5 md:py-[19px] px-5 md:px-7 rounded-[30px] primary-1-btn hover-animation"
-          >
-            불러오기
-          </button>
-        </section>
+              <button
+                type="button"
+                className="mt-9 flex items-center h-[50px] py-[13px] gap-2.5 md:py-[19px] px-5 md:px-7 rounded-[30px] primary-1-btn hover-animation"
+              >
+                불러오기
+              </button>
+            </section>
+          ))
+        )}
       </motion.div>
     </Overlay>
   );
