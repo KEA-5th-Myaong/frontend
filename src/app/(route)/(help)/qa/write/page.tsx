4cd7b5c0631392a41cebd1244efdfde091476e07
jiyeon <bjyy0201@gmail.com>
2024-11-07 16:43:40 +0900

    ✨ feat: [문의]: 문의 작성 페이지 구성
    - 문의 작성 페이지 세팅
    
    KAN-181

---
+++ src/app/(route)/(help)/qa/write/page.tsx
@@ -0,0 +1,65 @@
+'use client';
+
+import { useState } from 'react';
+import BackButton from '@/app/_components/BackButton';
+import Icons from '@/app/_components/ui/Icon';
+import { CheckIcon } from '@/app/_components/ui/iconPath';
+
+export default function QAWrite() {
+  const [isLock, setIsLock] = useState(false);
+  const [contentsData, setContentsData] = useState('');
+  const [countLength, setCountLength] = useState(0);
+
+  function handleContentChange(e: { target: { value: string } }) {
+    const { value } = e.target;
+    setContentsData(value);
+    setCountLength(value.length);
+  }
+
+  return (
+    <div className="flex-col w-full max-w-[1000px] min-w-[360px]: mx-10 md:my-11 my-32 ">
+      {/* 문의작성 title */}
+      <div className="flex-col h-24 py-1">
+        <BackButton />
+        <div className="relative flex w-full font-semibold text-3xl pt-7 py-10">
+          문의 작성
+          <div>
+            <div className="absolute right-2 font-light text-sm pt-10">
+              <div className="flex row-auto items-center">
+                <button
+                  type="button"
+                  className="relative p-1.5 border-2 border-black-1 mr-1 rounded-sm"
+                  onClick={() => {
+                    setIsLock((prev) => !prev);
+                  }}
+                >
+                  <div className="absolute top-0 left-0">
+                    {isLock && <Icons name={CheckIcon} className="flex w-3 h-3" />}
+                  </div>
+                </button>
+                비밀글로 작성
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="flex w-full py-8">
+        <textarea
+          className="resize-none px-[18px] pt-6 w-full border min-h-[76px] placeholder:text-gray-3 focus:outline-none text-lg"
+          placeholder="문의 제목을 입력해주세요."
+          maxLength={50}
+        />
+      </div>
+      <div className="flex w-full py-1">
+        <textarea
+          className="resize-none px-[18px] pt-6 w-full border h-[420px] placeholder:text-gray-3 focus:outline-none text-lg"
+          placeholder="문의 내용을 입력해주세요."
+          maxLength={1000}
+          onChange={handleContentChange}
+          value={contentsData}
+        />
+      </div>
+      <div>{countLength}/1000</div>
+    </div>
+  );
+}
