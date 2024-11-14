9d258b592a1f7a81d48f93aaab4890a5e45f7301
jiyeon <bjyy0201@gmail.com>
2024-11-14 10:12:58 +0900

    💄 design: [공지] : MoreIcon 제거
    KAN-181

--- src/app/(route)/(help)/notice/[noticeId]/select/page.tsx
+++ src/app/(route)/(help)/notice/[noticeId]/select/page.tsx
@@ -1,22 +1,9 @@
 'use client';
 
-import { useState, useRef } from 'react';
-import { MoreIcon } from '@/app/_components/ui/iconPath';
-import Icons from '@/app/_components/ui/Icon';
-import MoreOptions from '@/app/_components/MoreOptions';
 import testData from '../../test.json';
 import BackButton from '@/app/_components/BackButton';
-import useClickOutside from '@/app/_hooks/useClickOutside';
 
 export default function NoticeSelect() {
-  const [menuOpen, setMenuOpen] = useState(false);
-  const dropdownRef = useRef<HTMLDivElement>(null);
-
-  useClickOutside({
-    ref: dropdownRef,
-    callback: () => setMenuOpen(false),
-  });
-
   return (
     <div className="flex-col w-full max-w-[1000px] min-w-[360px]: mx-10 md:my-7 my-32">
       {/* 문의 title */}
@@ -28,21 +15,7 @@ export default function NoticeSelect() {
         </div>
       </div>
       <div className="flex-col w-full border-2 border-gray-5 pb-10">
-        <div className="relative py-8">
-          <div className="absolute  right-4 w-11 h-10 " ref={dropdownRef}>
-            <button
-              type="button"
-              className="cursor-pointer"
-              onClick={() => {
-                setMenuOpen((prev) => !prev);
-              }}
-            >
-              <Icons name={MoreIcon} />
-            </button>
-            {menuOpen && <MoreOptions />}
-          </div>
-        </div>
-        <div className="flex w-full pb-8 px-14 text-2xl font-semibold">
+        <div className="flex w-full mt-10 py-7 px-14 text-2xl font-semibold">
           <div className="">{testData.selectData.title}</div>
         </div>
         <div className="px-14">
