670f6ab542d024a0a03b3ad456e3bd1a085b312e
jiyeon <bjyy0201@gmail.com>
2024-11-18 13:14:56 +0900

    ♻️ refactor: [공지] 불필요한 문법 정리 및 수정사항 반영
    KAN-181

--- src/app/(route)/(help)/notice/[noticeId]/select/page.tsx
+++ src/app/(route)/(help)/notice/[noticeId]/select/page.tsx
@@ -5,8 +5,8 @@ import BackButton from '@/app/_components/BackButton';
 
 export default function NoticeSelect() {
   return (
-    <div className="flex-col w-full max-w-[1000px] min-w-[360px]: mx-10 md:my-7 my-32">
-      {/* 문의 title */}
+    <div className="flex-col w-full max-w-[1000px] min-w-[360px] mx-10 md:my-7 my-32">
+      {/* 공지 title */}
       <div className="flex-col w-full py-1">
         <BackButton />
         <div className="flex-col font-semibold text-3xl pt-7">
@@ -16,11 +16,11 @@ export default function NoticeSelect() {
       </div>
       <div className="flex-col w-full border-2 border-gray-5 pb-10">
         <div className="flex w-full mt-10 py-7 px-14 text-2xl font-semibold">
-          <div className="">{testData.selectData.title}</div>
+          <p className="">{testData.selectData.title}</p>
         </div>
         <div className="px-14">
           <p className="flex">{testData.selectData.content}</p>
-          <div className="py-5 text-gray-0">{testData.selectData.timestamp}</div>
+          <p className="py-5 text-gray-0">{testData.selectData.timestamp}</p>
         </div>
       </div>
     </div>
--- src/app/(route)/(help)/notice/list/page.tsx
+++ src/app/(route)/(help)/notice/list/page.tsx
@@ -7,46 +7,39 @@ export default function NoticeList() {
   const noticeListCount = testData.notices.length;
 
   return (
-    <div className="flex-col w-full h-hull max-w-[1000px] min-w-[360px]: mx-10 md:my-24 my-32">
-      <div className="flex row-auto">
-        <div className="flex h-24 w-[90%]">
-          {/* 문의게시판 title */}
-          <div className="font-semibold text-3xl">
-            공지 게시판
-            <div className="font-light text-sm py-4 text-gray-0">문의메일 : pplog@pplog.com</div>
-          </div>
+    <div className="w-full max-w-[1000px] min-w-[360px] mx-10 md:my-24 my-32">
+      <div className="flex h-24 w-[90%]">
+        {/* 게시판 title */}
+        <div className="font-semibold text-3xl">
+          공지 게시판
+          <div className="font-light text-sm py-4 text-gray-0">문의메일 : pplog@pplog.com</div>
         </div>
       </div>
 
       <div>
-        <div className="flex row-auto py-5 border-y-2 md:text-base text-sm  border-gray-0">
-          <div className="flex-center w-[10%] ">번호</div>
-          <div className="flex-center w-[65%] ">제목</div>
-          <div className="flex-center w-[25%] ">날짜</div>
+        <div className="flex py-5 border-y-2 md:text-base text-sm  border-gray-0">
+          <div className="flex-center w-[10%]">번호</div>
+          <div className="flex-center w-[65%]">제목</div>
+          <div className="flex-center w-[25%]">날짜</div>
         </div>
         <div>
           {testData.importantNotices.map((data) => (
-            <Link href="./1/select" className="flex w-full row-auto py-5 border-b-2 md:text-base text-sm border-gray-5">
-              <div className="flex-center w-[10%] ">{data.noticeId}</div>
-              <div className="relative flex-center w-[65%] ">
+            <Link href="./1/select" className="flex w-full py-5 border-b-2 md:text-base text-sm border-gray-5">
+              <div className="flex-center w-[10%]">{data.noticeId}</div>
+              <div className="relative flex-center w-[65%]">
                 <div className="absolute left-20">
                   <div className="flex-center w-10 h-5 rounded-lg bg-red-0 text-xs text-white-0">중요</div>
                 </div>
-                {data.title}
+                <p>{data.title}</p>
               </div>
-
-              <div className="flex-center w-[25%] ">{data.timestamp}</div>
+              <div className="flex-center w-[25%]">{data.timestamp}</div>
             </Link>
           ))}
           {testData.notices.map((data) => (
-            <Link href="./1/select" className="flex w-full row-auto py-5 border-b-2 md:text-base text-sm border-gray-5">
-              <div className="flex-center w-[10%] ">{data.noticeId}</div>
-              <div className="relative flex-center w-[65%] ">
-                <div className="absolute left-20" />
-                {data.title}
-              </div>
-
-              <div className="flex-center w-[25%] ">{data.timestamp}</div>
+            <Link href="./1/select" className="flex w-full py-5 border-b-2 md:text-base text-sm border-gray-5">
+              <p className="flex-center w-[10%]">{data.noticeId}</p>
+              <p className="flex-center w-[65%]">{data.title}</p>
+              <p className="flex-center w-[25%]">{data.timestamp}</p>
             </Link>
           ))}
         </div>
