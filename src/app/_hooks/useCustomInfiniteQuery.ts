751f579c7dc6c915beeff537f0ea7b78eca442ba
kimgorok <kjyse2@naver.com>
2024-11-24 14:26:27 +0900

    🐛fix: [블로그] useCustomInfiniteQuery.ts 타입 오류 해결
    - 기본 값으로 initailPageParam 추가
    - queryFn과 options에 추가 타입 지정
    
    KAN-224

--- src/app/_hooks/useCustomInfiniteQuery.ts
+++ src/app/_hooks/useCustomInfiniteQuery.ts
@@ -1,19 +1,29 @@
-import { useInfiniteQuery, UseInfiniteQueryOptions } from '@tanstack/react-query';
+import {
+  useInfiniteQuery, // 무한 스크롤 쿼리를 위한 기본 훅
+  UseInfiniteQueryOptions, // 무한 스크롤 쿼리 옵션 타입
+  QueryFunctionContext, // 쿼리 함수의 컨텍스트 타입
+  InfiniteData, // 무한 스크롤 데이터를 위한 타입
+} from '@tanstack/react-query';
 
 const defaultQueryOptions = {
   refetchOnMount: false,
   refetchOnWindowFocus: false,
-};
+  initialPageParam: null,
+} as const;
 
 export default function useCustomInfiniteQuery<TData>(
   queryKey: string[],
-  queryFn: (context: { pageParam: string }) => Promise<TData>,
-  options?: Omit<UseInfiniteQueryOptions<TData, Error, TData>, 'queryKey' | 'queryFn'>,
+  queryFn: (context: QueryFunctionContext<string[], unknown>) => Promise<TData>,
+  options?: Omit<
+    UseInfiniteQueryOptions<TData, Error, InfiniteData<TData>, TData, string[], unknown>,
+    'queryKey' | 'queryFn'
+  >,
 ) {
-  return useInfiniteQuery<TData, Error, TData>({
+  return useInfiniteQuery<TData, Error, InfiniteData<TData>, string[]>({
     queryKey,
-    queryFn,
-    ...defaultQueryOptions,
+    queryFn, // 데이터를 가져오는 함수
+    getNextPageParam: () => undefined, // 다음 페이지 파라미터 계산 함수 (기본값: undefined)
+    ...defaultQueryOptions, // 기본 옵션 적용
     ...options,
   });
 }
