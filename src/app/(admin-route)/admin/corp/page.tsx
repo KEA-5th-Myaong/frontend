1511893d3312acdc363c7ef9d69de898e0041a97
jiyeon <bjyy0201@gmail.com>
2024-11-26 12:09:15 +0900

    💄 design: [관리자-기업관리] : UI 디자인 구현
    - 아이콘 및 디자인 추가
    
    KAN-233

--- src/app/(admin-route)/admin/corp/page.tsx
+++ src/app/(admin-route)/admin/corp/page.tsx
@@ -1,5 +1,5 @@
 import Icons from '@/app/_components/ui/Icon';
-import { SearchIcon, ArrowCircleIcon } from '@/app/_components/ui/iconPath';
+import { SearchIcon, ArrowCircleIcon, TrashIcon, EditIcon } from '@/app/_components/ui/iconPath';
 import testData from './test.json';
 
 export default function AdminCorp() {
@@ -15,12 +15,22 @@ export default function AdminCorp() {
           </div>
         </div>
         <div className="flex-col mt-2 h-[600px] max-w-[40%] border-gray-5 border">
-          <div className="flex px-14 bg-gray-5 py-5 w-full">기업명</div>
-          <div>
+          <div className="flex px-12 bg-gray-5 py-4 w-full text-sm font-semibold">기업명</div>
+          <div className="my-2">
             {testData.corpList.map((item) => (
-              <div className="flex items-center w-full">
-                <Icons name={ArrowCircleIcon} className="flex justify-center w-4 h-4" />
-                {item.name}
+              <div className="flex items-center w-full h-9">
+                <Icons name={ArrowCircleIcon} className="flex justify-center h-4 w-[10%]" />
+                <div className="flex w-[76%] item-center">
+                  {item.name}
+                  <Icons
+                    name={{
+                      ...EditIcon,
+                      options: { ...EditIcon.options, stroke: '#757575', strokeWidth: 2 },
+                    }}
+                    className="h-5 ml-2"
+                  />
+                </div>
+                <Icons name={TrashIcon} className="h-4 w-[10%]" />
               </div>
             ))}
           </div>
