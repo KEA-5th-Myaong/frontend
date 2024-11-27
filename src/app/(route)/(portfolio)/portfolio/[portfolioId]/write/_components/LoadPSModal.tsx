7400285b0a493598bcc4c79f51624dc081e2bcb0
yeonilil <gwagseoyeon@gmail.com>
2024-11-27 12:17:54 +0900

    🐛 fix: 자기소개서 불러오기 API 재연결

--- src/app/(route)/(portfolio)/portfolio/[portfolioId]/write/_components/LoadPSModal.tsx
+++ src/app/(route)/(portfolio)/portfolio/[portfolioId]/write/_components/LoadPSModal.tsx
@@ -1,13 +1,14 @@
 'use client';
 
 import { motion } from 'framer-motion';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { modalMotion } from '@/app/_components/Modal';
 import Overlay from '@/app/_components/Overlay';
 import { fetchPSList } from '@/app/(route)/(personal-statement)/_services/psServices';
 import useCustomQuery from '@/app/_hooks/useCustomQuery';
 import { PSListBoxProps } from '@/app/(route)/(personal-statement)/personal-statement/[id]/list/_types/psList';
 import Image from 'next/image';
+import Link from 'next/link';
 
 interface LoadPSModalProps {
   onOverlayClick?: () => void;
@@ -21,35 +22,43 @@ export default function LoadPSModal({ onOverlayClick }: LoadPSModalProps) {
   };
 
   // 자기소개서 목록 불러오기
-  const [personalStatement, setPersonalStatement] = useState<PSListBoxProps[]>([]);
-  const { data: psListData } = useCustomQuery(['psList'], () => fetchPSList());
+  const [psList, setPSList] = useState([]); // 자소서 배열
+  const { data: psData } = useCustomQuery(['ps'], () => fetchPSList());
+
+  useEffect(() => {
+    if (psData?.data && Array.isArray(psData.data)) {
+      setPSList(psData.data.slice(0, 5)); // 자소서 목록 로드
+    }
+  }, [psData]);
+
+  // FIX: 자기소개서 없을 때 작성하러 가기 링크 수정 필요
 
   return (
     <Overlay onClick={handleOverlayClick}>
       <motion.div
         onClick={(e) => e.stopPropagation()} // 클릭 이벤트가 Overlay까지 전달되지 않도록
         {...modalMotion}
-        className="flex m-4 min-w-[300px]  max-w-[1000px] hide-scrollbar overflow-y-scroll w-full pb-6 px-[65px] pt-[58px] flex-col items-start gap-6 rounded-2xl bg-white-0 shadow-md"
+        className="flex m-4 min-w-[300px]  max-w-[1000px] max-h-[600px] hide-scrollbar overflow-y-scroll w-full pb-6 px-[65px] pt-[58px] flex-col items-start gap-6 rounded-2xl bg-white-0 shadow-md"
       >
         <h1 className="pre-2xl-semibold">자기소개서 불러오기</h1>
-        {personalStatement.length === 0 ? (
+        {psList.length === 0 ? (
           <>
             <div className="flex flex-col gap-3 mt-10 ml-[350px]">
               <Image className="animate-bounce" src="/mascot.png" alt="마스코트" width={204} height={193} />
               <p className="text-gray-0">자기소개서를 작성해보세요</p>
             </div>
-            {/* <Link href={`personal-statement/{id}/create`}> <- 작성 화면으로 이동 추가*/}
-            <button
-              type="button"
-              className=" ml-[370px] flex items-center h-[50px] py-[13px] gap-2.5 md:py-[19px] px-5 md:px-7 rounded-[30px] primary-1-btn hover-animation"
-            >
-              작성하러 가기
-            </button>
-            {/* </Link> */}
+            <Link href={`/personal-statement/member2/list`}>
+              <button
+                type="button"
+                className=" ml-[370px] flex items-center h-[50px] py-[13px] gap-2.5 md:py-[19px] px-5 md:px-7 rounded-[30px] primary-1-btn hover-animation"
+              >
+                작성하러 가기
+              </button>
+            </Link>
           </>
         ) : (
-          personalStatement.map((ps) => (
-            <section className="flex justify-between px-6 pb-4 pt-6 sm:px-8 sm:pb-4 sm:pt-10 border border-gray-2 rounded-lg bg-white-0 cursor-pointer">
+          psList?.map((ps: PSListBoxProps) => (
+            <section className="flex justify-between w-full max-h-[400px] px px-6 pb-4 pt-6 sm:px-8 sm:pb-4 sm:pt-10 border border-gray-2 rounded-lg bg-white-0 cursor-pointer">
               <div>
                 <div className="flex items-center gap-4">
                   <p className="font-semibold text-lg sm:text-xl line-clamp-2">{ps.title}</p>
