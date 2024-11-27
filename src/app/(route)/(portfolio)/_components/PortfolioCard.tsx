ff9fce49421c9049d69643904fd7262025f177b5
yeonilil <gwagseoyeon@gmail.com>
2024-11-27 12:46:31 +0900

    🐛 fix: 메모 클릭 시 링크 이동되는 문제 수정

--- src/app/(route)/(portfolio)/_components/PortfolioCard.tsx
+++ src/app/(route)/(portfolio)/_components/PortfolioCard.tsx
@@ -7,6 +7,7 @@ import PortfolioDropdown from './PortfolioDropdown';
 import useClickOutside from '@/app/_hooks/useClickOutside';
 import { PortfolioCardProps } from '@/app/_types/portfolio';
 import usePortfolioStore from '@/app/_store/portfolio';
+import Link from 'next/link';
 
 export default function PortfolioCard({ id, title, date, memo }: PortfolioCardProps) {
   const [isShowDropdown, setIsShowDropdown] = useState(false);
@@ -33,28 +34,30 @@ export default function PortfolioCard({ id, title, date, memo }: PortfolioCardPr
 
   return (
     <div className=" relative w-[320px] bg-white-0 border border-gray-5 rounded-xl pt-[30px] pb-2.5 px-[30px]">
-      <div className="flex justify-between">
-        <h1 className="max-w-[15ch] font-semibold whitespace-nowrap text-ellipsis overflow-hidden">{title}</h1>
-        <button
-          type="button"
-          onClick={handleSetMain}
-          className={`${isMainPortfolio === id ? 'bg-primary-4' : 'bg-gray-5'} flex-center 
+      <Link href={`/`}>
+        <div className="flex justify-between">
+          <h1 className="max-w-[15ch] font-semibold whitespace-nowrap text-ellipsis overflow-hidden">{title}</h1>
+          <button
+            type="button"
+            onClick={handleSetMain}
+            className={`${isMainPortfolio === id ? 'bg-primary-4' : 'bg-gray-5'} flex-center 
           px-2.5 rounded-[5px] font-semibold text-[11px] text-white-0`}
-        >
-          대표
-        </button>
-        <div ref={dropdownRef}>
-          <Icons
-            name={MoreIcon}
-            onClick={() => {
-              setIsShowDropdown((prev) => !prev);
-            }}
-            className="cursor-pointer"
-          />
-        </div>
+          >
+            대표
+          </button>
+          <div ref={dropdownRef}>
+            <Icons
+              name={MoreIcon}
+              onClick={() => {
+                setIsShowDropdown((prev) => !prev);
+              }}
+              className="cursor-pointer"
+            />
+          </div>
 
-        {isShowDropdown && <PortfolioDropdown />}
-      </div>
+          {isShowDropdown && <PortfolioDropdown />}
+        </div>
+      </Link>
       <form className="bg-gray-4 rounded-md mt-5 py-[15px] px-[15px]">
         <h1 className="font-semibold text-sm">MEMO</h1>
         <input
--- src/app/(route)/(portfolio)/portfolio/page.tsx
+++ src/app/(route)/(portfolio)/portfolio/page.tsx
@@ -1,72 +1,23 @@
 'use client';
 
 import Link from 'next/link';
-import { useQueryClient } from '@tanstack/react-query';
 import Icons from '../../../_components/ui/Icon';
 import { PlusIcon } from '../../../_components/ui/iconPath';
 import PortfolioCard from '../_components/PortfolioCard';
 import PortfolioAddCard from '../_components/PortfolioAddCard';
 import usePortfolioStore from '../../../_store/portfolio';
 import { PortfolioCardProps } from '../../../_types/portfolio';
-import useCustomQuery from '@/app/_hooks/useCustomQuery';
-import { fetchPortfolios, postPorfolios } from '../_services/portfolioServices';
-import useCustomMutation from '@/app/_hooks/useCustomMutation';
-
 export default function PortfolioList() {
   const { portfolio } = usePortfolioStore();
 
-  const { data: pfData } = useCustomQuery(['pf'], () => fetchPortfolios());
-
-  console.log(pfData);
-
-  const queryClient = useQueryClient();
-  const createPortfolioMutation = useCustomMutation(postPorfolios, {
-    onSuccess: (data) => {
-      console.log('반환 받은 포트폴리오 아이디', data);
-      queryClient.invalidateQueries(['pf']); // 캐시 초기화
-    },
-  });
-  const handlePostTest = () => {
-    const newPortfolio = {
-      title: '제목',
-      preferredJob: '관심 직무',
-      tel: '010-0000-0000',
-      email: '이메일@주소',
-      educations: [
-        {
-          name: '학교 이름',
-          major: '전공',
-          graduation: '0000-00',
-          gpa: null,
-        },
-      ],
-      picUrl: null,
-      experiences: [
-        {
-          name: '기업 이름',
-          start: '0000-00-00',
-          end: null,
-          position: null,
-          achievement: null,
-        },
-      ],
-      ps: null,
-      links: [],
-      skills: [],
-      certifications: [],
-      extraActivities: [
-        {
-          name: '활동명',
-          start: '0000-00-00',
-          end: null,
-          institution: '기관 이름',
-          description: null,
-        },
-      ],
-    };
-
-    createPortfolioMutation.mutate(newPortfolio);
-  };
+  // const { data: pfData } = useCustomQuery(['pf'], () => fetchPortfolios());
+  // const queryClient = useQueryClient();
+  // const createPortfolioMutation = useCustomMutation(postPorfolios, {
+  //   onSuccess: (data) => {
+  //     console.log('반환 받은 포트폴리오 아이디', data);
+  //     queryClient.invalidateQueries(['pf']); // 캐시 초기화
+  //   },
+  // });
   return (
     <div className="flex flex-col items-center px-[50px] min-w-[360px] w-full pt-14 md:pt-0">
       <div className="pt-[60px] flex justify-between items-center w-full max-w-[1000px] md:px-[60px] lg:px-[50px] ">
@@ -91,9 +42,7 @@ export default function PortfolioList() {
       <div className="flex flex-col items-center mt-[30px] mb-[100px] px-[50px]">
         <div className="w-full lg:max-w-[1000px] grid justify-center sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-5 gap-y-[25px] gap-4">
           {portfolio.map((item: PortfolioCardProps) => (
-            <Link href={`/portfolio/${item.id}/write`}>
-              <PortfolioCard key={item.id} id={item.id} title={item.title} date="2024.10.17" />
-            </Link>
+            <PortfolioCard key={item.id} id={item.id} title={item.title} date="2024.10.17" />
           ))}
           <PortfolioAddCard />
         </div>
