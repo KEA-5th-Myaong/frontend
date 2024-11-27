ae21744306d01ed761ea803bd70e9851eedfbbd4
jiyeon <bjyy0201@gmail.com>
2024-11-27 17:27:33 +0900

    💄 design: [관리자-기업관리] : 피그마 사이즈 반영
    - 아이콘 및 div 크기 수정
    
    KAN-233

--- src/app/(admin-route)/admin/corp/page.tsx
+++ src/app/(admin-route)/admin/corp/page.tsx
@@ -1,8 +1,25 @@
+'use client';
+
+import { useState } from 'react';
 import Icons from '@/app/_components/ui/Icon';
 import { SearchIcon, ArrowCircleIcon, TrashIcon, EditIcon } from '@/app/_components/ui/iconPath';
 import testData from './test.json';
+import { initailModalState } from '@/app/_components/Modal';
 
 export default function AdminCorp() {
+  const [editModalOpen, setEditModalOpen] = useState(initailModalState);
+
+  function handleEditModal() {
+    console.log(editModalOpen);
+    setEditModalOpen((prev) => ({
+      ...prev,
+      open: true,
+      topText: '숨김 처리가 해제 되었습니다.',
+      btnText: '확인',
+      onBtnClick: () => setEditModalOpen(initailModalState),
+    }));
+  }
+
   return (
     <div className="flex gap-2.5 w-full pl-12 pb-40">
       <div className="w-full min-w-[545px] px-6 py-8 bg-white-0 border border-gray-5">
@@ -14,23 +31,25 @@ export default function AdminCorp() {
             <Icons name={{ ...SearchIcon, fill: '#00000' }} />
           </div>
         </div>
-        <div className="flex-col mt-2 h-[600px] max-w-[40%] border-gray-5 border">
+        <div className="flex-col mt-2 h-[600px] max-w-[50%] border-gray-5 border">
           <div className="flex px-12 bg-gray-5 py-4 w-full text-sm font-semibold">기업명</div>
           <div className="my-2">
             {testData.corpList.map((item) => (
-              <div className="flex items-center w-full h-9">
-                <Icons name={ArrowCircleIcon} className="flex justify-center h-4 w-[10%]" />
+              <div className="flex items-center w-full h-11">
+                <Icons name={ArrowCircleIcon} className="flex justify-center h-5 w-[10%]" />
                 <div className="flex w-[76%] item-center">
                   {item.name}
-                  <Icons
-                    name={{
-                      ...EditIcon,
-                      options: { ...EditIcon.options, stroke: '#757575', strokeWidth: 2 },
-                    }}
-                    className="h-5 ml-2"
-                  />
+                  <button type="button" onClick={handleEditModal} className="flex gap-1.5 items-center">
+                    <Icons
+                      name={{
+                        ...EditIcon,
+                        options: { ...EditIcon.options, stroke: '#757575', strokeWidth: 2 },
+                      }}
+                      className="h-full ml-3"
+                    />
+                  </button>
                 </div>
-                <Icons name={TrashIcon} className="h-4 w-[10%]" />
+                <Icons name={TrashIcon} className="h-6 w-[10%]" />
               </div>
             ))}
           </div>
