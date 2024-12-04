9dfb261ea1a5401ef16c06a96610a944be9954da
jiyeon <bjyy0201@gmail.com>
2024-12-04 14:52:29 +0900

    💄 design: [관리자-기업관리] : 기업 생성 추가
    - 기업 생성 칸 추가 및 UI 구현
    
    KAN-233

--- src/app/(admin-route)/admin/corp/page.tsx
+++ src/app/(admin-route)/admin/corp/page.tsx
@@ -1,24 +1,12 @@
 'use client';
 
-import { useState } from 'react';
 import Icons from '@/app/_components/ui/Icon';
 import { SearchIcon, ArrowCircleIcon, TrashIcon, EditIcon } from '@/app/_components/ui/iconPath';
 import testData from './test.json';
-import { initailModalState } from '@/app/_components/Modal';
 
 export default function AdminCorp() {
-  const [editModalOpen, setEditModalOpen] = useState(initailModalState);
-
-  function handleEditModal() {
-    console.log(editModalOpen);
-    setEditModalOpen((prev) => ({
-      ...prev,
-      open: true,
-      topText: '숨김 처리가 해제 되었습니다.',
-      btnText: '확인',
-      onBtnClick: () => setEditModalOpen(initailModalState),
-    }));
-  }
+  // 임시* 이름 편집 함수
+  function handleEdit() {}
 
   return (
     <div className="flex gap-2.5 w-full pl-12 pb-40">
@@ -31,27 +19,38 @@ export default function AdminCorp() {
             <Icons name={{ ...SearchIcon, fill: '#00000' }} />
           </div>
         </div>
-        <div className="flex-col mt-2 h-[600px] max-w-[50%] border-gray-5 border">
+        <div className="flex-col mt-2 h-[500px] max-w-[50%] border-gray-5 border">
           <div className="flex px-12 bg-gray-5 py-4 w-full text-sm font-semibold">기업명</div>
-          <div className="my-2">
-            {testData.corpList.map((item) => (
-              <div className="flex items-center w-full h-11">
-                <Icons name={ArrowCircleIcon} className="flex justify-center h-5 w-[10%]" />
-                <div className="flex w-[76%] item-center">
-                  {item.name}
-                  <button type="button" onClick={handleEditModal} className="flex gap-1.5 items-center">
-                    <Icons
-                      name={{
-                        ...EditIcon,
-                        options: { ...EditIcon.options, stroke: '#757575', strokeWidth: 2 },
-                      }}
-                      className="h-full ml-3"
-                    />
-                  </button>
+          <div className="my-2 h-full">
+            <div className="flex-col h-[75%]">
+              {testData.corpList.map((item) => (
+                <div className="flex items-center w-full h-11">
+                  <Icons name={ArrowCircleIcon} className="flex justify-center h-5 w-[15%]" />
+                  <div className="flex w-[74%] item-center">
+                    {item.name}
+                    <button type="button" onClick={handleEdit} className="flex gap-1.5 items-center">
+                      <Icons
+                        name={{
+                          ...EditIcon,
+                          options: { ...EditIcon.options, stroke: '#757575', strokeWidth: 2 },
+                        }}
+                        className="h-full ml-3"
+                      />
+                    </button>
+                  </div>
+                  <Icons name={TrashIcon} className="h-6 w-[10%]" />
                 </div>
-                <Icons name={TrashIcon} className="h-6 w-[10%]" />
-              </div>
-            ))}
+              ))}
+            </div>
+            <div className="flex h-[10%] items-center px-5">
+              <textarea
+                className="resize-none border-gray-2 px-[18px] py-2.5 w-[75%] border max-h-[45px] rounded-xl placeholder:text-gray-3 focus:outline-none"
+                placeholder="기업명"
+              />
+              <button type="button" className="w-[23%] ml-2 rounded-xl text-sm bg-black-3 text-white-0 h-full ">
+                기업생성
+              </button>
+            </div>
           </div>
         </div>
       </div>
