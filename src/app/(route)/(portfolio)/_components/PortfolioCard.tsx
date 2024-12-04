f43179f45f184e7bdb0f09efe7583b199e7581f4
yeonilil <gwagseoyeon@gmail.com>
2024-12-05 01:04:44 +0900

    🐛 fix: 링크를 제목에 한정하여 대표 정상적으로 동작 되도록 수정

--- src/app/(route)/(portfolio)/_components/PortfolioCard.tsx
+++ src/app/(route)/(portfolio)/_components/PortfolioCard.tsx
@@ -34,30 +34,32 @@ export default function PortfolioCard({ id, title, date, memo }: PortfolioCardPr
 
   return (
     <div className=" relative w-[320px] bg-white-0 border border-gray-5 rounded-xl pt-[30px] pb-2.5 px-[30px]">
-      <Link href={`/portfolio/${id}/write`}>
-        <div className="flex justify-between">
-          <h1 className="max-w-[15ch] font-semibold whitespace-nowrap text-ellipsis overflow-hidden">{title}</h1>
-          <button
-            type="button"
-            onClick={handleSetMain}
-            className={`${isMainPortfolio === id ? 'bg-primary-4' : 'bg-gray-5'} flex-center 
+      <div className="flex justify-between">
+        <Link href={`/portfolio/${id}/write`}>
+          <h1 className="max-w-[15ch] font-semibold whitespace-nowrap text-ellipsis overflow-hidden hover:text-gray-500">
+            {title}
+          </h1>
+        </Link>
+        <button
+          type="button"
+          onClick={handleSetMain}
+          className={`${isMainPortfolio === id ? 'bg-primary-1' : 'bg-gray-5'} flex-center 
           px-2.5 rounded-[5px] font-semibold text-[11px] text-white-0`}
-          >
-            대표
-          </button>
-          <div ref={dropdownRef}>
-            <Icons
-              name={MoreIcon}
-              onClick={() => {
-                setIsShowDropdown((prev) => !prev);
-              }}
-              className="cursor-pointer"
-            />
-          </div>
-
-          {isShowDropdown && <PortfolioDropdown />}
+        >
+          대표
+        </button>
+        <div ref={dropdownRef}>
+          <Icons
+            name={MoreIcon}
+            onClick={() => {
+              setIsShowDropdown((prev) => !prev);
+            }}
+            className="cursor-pointer"
+          />
         </div>
-      </Link>
+
+        {isShowDropdown && <PortfolioDropdown />}
+      </div>
       <form className="bg-gray-4 rounded-md mt-5 py-[15px] px-[15px]">
         <h1 className="font-semibold text-sm">MEMO</h1>
         <input
