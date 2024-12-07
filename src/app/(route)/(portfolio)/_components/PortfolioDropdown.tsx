da090b076e1e08a007fa980926f843da1ba63345
yeonilil <gwagseoyeon@gmail.com>
2024-12-07 17:42:14 +0900

    ✨ feat: 포트폴리오 삭제, 작성 API 연결

--- src/app/(route)/(portfolio)/_components/PortfolioDropdown.tsx
+++ src/app/(route)/(portfolio)/_components/PortfolioDropdown.tsx
@@ -1,8 +1,26 @@
-export default function PortfolioDropdown() {
+interface PortfolioDropdownProps {
+  id: string;
+  onDelete: (id: string) => void;
+}
+
+export default function PortfolioDropdown({ id, onDelete }: PortfolioDropdownProps) {
   return (
-    <div className="absolute flex-center flex-col right-[30px] top-[60px] p-2.5 bg-white-0 text-sm rounded-lg border  border-gray-5">
+    <div
+      onClick={(e) => e.stopPropagation()} // 이벤트 버블링 방지
+      className="absolute flex-center flex-col right-[30px] top-[60px] p-2.5 bg-white-0 text-sm rounded-lg border  border-gray-5"
+    >
       <div className="dropdown-item">수정</div>
-      <div className="dropdown-item">삭제</div>
+      <button
+        type="button"
+        onClick={(e) => {
+          e.stopPropagation(); // 이벤트 버블링 방지
+          console.log('Delete button clicked'); // 디버깅 메시지
+          onDelete(id); // 삭제 함수 호출
+        }}
+        className="dropdown-item"
+      >
+        삭제
+      </button>
       <div className="dropdown-item">공유</div>
     </div>
   );
