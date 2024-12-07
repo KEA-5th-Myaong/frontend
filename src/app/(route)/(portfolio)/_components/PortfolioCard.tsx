6e7907499e41b4fc1e151514609d9028f2aa1586
yeonilil <gwagseoyeon@gmail.com>
2024-12-07 17:42:14 +0900

    ✨ feat: 포트폴리오 삭제, 작성 API 연결

--- src/app/(route)/(portfolio)/_components/PortfolioCard.tsx
+++ src/app/(route)/(portfolio)/_components/PortfolioCard.tsx
@@ -8,18 +8,18 @@ import PortfolioDropdown from './PortfolioDropdown';
 import useClickOutside from '@/app/_hooks/useClickOutside';
 import { PortfolioCardProps } from '@/app/_types/portfolio';
 import usePortfolioStore from '@/app/_store/portfolio';
-import { postPortfoliosMemo, putPortfoliosMain } from '../_services/portfolioServices';
+import { deletePortfolios, postPortfoliosMemo, putPortfoliosMain } from '../_services/portfolioServices';
 
 export default function PortfolioCard({ portfolioId, portfolioName, timestamp, memo }: PortfolioCardProps) {
   const [isShowDropdown, setIsShowDropdown] = useState(false);
   const { isMainPortfolio, setMainPortfolio, setMemo } = usePortfolioStore();
   const [currentMemo, setCurrentMemo] = useState(memo); // 메모의 현재 상태를 저장
 
-  const dropdownRef = useRef<HTMLDivElement>(null);
-  useClickOutside({
-    ref: dropdownRef,
-    callback: () => setIsShowDropdown(false),
-  });
+  // const dropdownRef = useRef<HTMLDivElement>(null);
+  // useClickOutside({
+  //   ref: dropdownRef,
+  //   callback: () => setIsShowDropdown(false),
+  // });
 
   // 대표 포트폴리오 설정
   const handleSetMain = async () => {
@@ -38,6 +38,20 @@ export default function PortfolioCard({ portfolioId, portfolioName, timestamp, m
     await postPortfoliosMemo(portfolioId, memo);
   };
 
+  // 포트폴리오 목록에서 삭제
+  const handleDeletePortfolio = async () => {
+    console.log('Deleting portfolio...'); // 디버깅 메시지 추가
+    try {
+      await deletePortfolios(portfolioId); // API 호출
+      console.log(`Portfolio with ID ${portfolioId} deleted`);
+    } catch (error) {
+      console.error('Error deleting portfolio:', error);
+      alert('포트폴리오 삭제에 실패했습니다.');
+    } finally {
+      setIsShowDropdown(false); // 항상 마지막에 닫기
+    }
+  };
+
   return (
     <div className=" relative w-[320px] bg-white-0 border border-gray-5 rounded-xl pt-[30px] pb-2.5 px-[30px]">
       <div className="flex justify-between">
@@ -54,7 +68,7 @@ export default function PortfolioCard({ portfolioId, portfolioName, timestamp, m
         >
           대표
         </button>
-        <div ref={dropdownRef}>
+        <div>
           <Icons
             name={MoreIcon}
             onClick={() => {
@@ -64,7 +78,7 @@ export default function PortfolioCard({ portfolioId, portfolioName, timestamp, m
           />
         </div>
 
-        {isShowDropdown && <PortfolioDropdown />}
+        {isShowDropdown && <PortfolioDropdown id={portfolioId} onDelete={handleDeletePortfolio} />}
       </div>
       <form className="bg-gray-4 rounded-md mt-5 py-[15px] px-[15px]">
         <h1 className="font-semibold text-sm">MEMO</h1>
