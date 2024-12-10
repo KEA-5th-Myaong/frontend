b9a9530b65487751dbad5d33952b3d05b7e69bb1
yeonilil <gwagseoyeon@gmail.com>
2024-12-10 15:55:17 +0900

    ✨ feat: 포트폴리오 조회 페이지 드롭다운 수정, 삭제 연결

--- src/app/(route)/(portfolio)/_components/PortfolioDropdown.tsx
+++ src/app/(route)/(portfolio)/_components/PortfolioDropdown.tsx
@@ -1,20 +1,26 @@
 interface PortfolioDropdownProps {
   id: string;
   onDelete: (id: string) => void;
+  onModify: (id: string) => void;
 }
 
-export default function PortfolioDropdown({ id, onDelete }: PortfolioDropdownProps) {
+export default function PortfolioDropdown({ id, onDelete, onModify }: PortfolioDropdownProps) {
   return (
-    <div
-      onClick={(e) => e.stopPropagation()} // 이벤트 버블링 방지
-      className="absolute flex-center flex-col right-[30px] top-[60px] p-2.5 bg-white-0 text-sm rounded-lg border  border-gray-5"
-    >
-      <div className="dropdown-item">수정</div>
+    <div className="absolute flex-center flex-col right-[30px] top-[60px] p-2.5 bg-white-0 text-sm rounded-lg border  border-gray-5">
       <button
         type="button"
         onClick={(e) => {
           e.stopPropagation(); // 이벤트 버블링 방지
-          console.log('Delete button clicked'); // 디버깅 메시지
+          onModify(id); // 수정 함수 호출
+        }}
+        className="dropdown-item"
+      >
+        수정
+      </button>
+      <button
+        type="button"
+        onClick={(e) => {
+          e.stopPropagation(); // 이벤트 버블링 방지
           onDelete(id); // 삭제 함수 호출
         }}
         className="dropdown-item"
