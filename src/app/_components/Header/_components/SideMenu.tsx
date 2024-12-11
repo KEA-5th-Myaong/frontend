80d33827564a720d63724798a8ab7714e4682be5
kimgorok <kjyse2@naver.com>
2024-12-11 13:11:48 +0900

    ✨feat: 사이드메뉴에 로그아웃 버튼 추가
    - handleLogout 함수 적용
    
    KAN-258

--- src/app/_components/Header/_components/SideMenu.tsx
+++ src/app/_components/Header/_components/SideMenu.tsx
@@ -3,8 +3,11 @@
 import { AnimatePresence, motion } from 'framer-motion';
 import Image from 'next/image';
 import Link from 'next/link';
-import Overlay from '../../Overlay';
+import Cookies from 'js-cookie';
+import { useQueryClient } from '@tanstack/react-query';
+import { useRouter } from 'next/navigation';
 import { User } from '@/app/_hooks/useMe';
+import Overlay from '../../Overlay';
 
 interface SideMenuProps {
   isOpen: boolean;
@@ -14,6 +17,26 @@ interface SideMenuProps {
 
 export default function SideMenu({ isOpen, onClose, userData }: SideMenuProps) {
   const isLogined = userData?.data?.nickname;
+  const router = useRouter();
+
+  const queryClient = useQueryClient();
+
+  const handleLogout = async () => {
+    try {
+      // 로그아웃 처리
+      Cookies.remove('accessToken');
+      // 모든 관련 쿼리 무효화
+      await queryClient.invalidateQueries({ queryKey: ['me'] });
+
+      // 캐시된 사용자 데이터 제거
+      queryClient.setQueryData(['me'], null);
+      // 다른 관련된 쿼리들도 초기화
+      queryClient.clear();
+      router.push('/log-in');
+    } catch (error) {
+      console.error('로그아웃 중 오류 발생:', error);
+    }
+  };
   return (
     <AnimatePresence>
       {isOpen && (
@@ -21,7 +44,7 @@ export default function SideMenu({ isOpen, onClose, userData }: SideMenuProps) {
           <motion.div
             animate={{ x: 0.1 }} // 나타날 때 왼쪽에서 오른쪽으로 슬라이드
             transition={{ duration: 0.1 }} // 전환 애니메이션 속도
-            className="fixed top-0 -left-1 w-52 pl-4 h-full x-10 z-40 bg-white-0 text-black-0 transition-transform transform drop-shadow-md"
+            className="fixed top-0 -left-1 w-52 pl-4 min-h-full x-10 z-40 bg-white-0 text-black-0 transition-transform transform drop-shadow-md"
           >
             <div>
               <div className="flex w-full">
@@ -87,6 +110,9 @@ export default function SideMenu({ isOpen, onClose, userData }: SideMenuProps) {
                   <p className="text-sm text-gray-0 pt-2 pl-5">내 포트폴리오</p>
                 </Link>
               </div>
+            </div>
+            <div onClick={handleLogout} className="w-full flex-center pr-8 mt-12 text-sm text-gray-3">
+              로그아웃
             </div>
           </motion.div>
         </Overlay>
