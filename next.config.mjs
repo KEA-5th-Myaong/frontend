5236c288e5832a9f22e688549a00f137d3cce795
yeonilil <gwagseoyeon@gmail.com>
2024-12-12 15:02:44 +0900

    🐛 fix: 도메인 env 변경

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: [process.env.DOMAIN], // 허용할 도메인 추가
+    domains: [process.env.NEXT_PUBLIC_DOMAIN], // 허용할 도메인 추가
   },
 };
 
