b5f84db7823881a76c2fe5ae997899b7e00dd91f
yeonilil <gwagseoyeon@gmail.com>
2024-12-12 14:34:27 +0900

    🐛 fix: next.config.mjs 도메인으로 변경

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['172.16.210.30'], // 허용할 도메인 추가
+    domains: [process.env.DOMAIN], // 허용할 도메인 추가
   },
 };
 
