d403e393aa6be805885af856480658ac21514e46
yeonilil <gwagseoyeon@gmail.com>
2024-12-13 13:04:00 +0900

    ✨ feat: 알림 무한스크롤 구현

--- src/app/_components/Header/_components/Alarm.tsx
+++ src/app/_components/Header/_components/Alarm.tsx
@@ -1,20 +1,37 @@
 import { useQueryClient } from '@tanstack/react-query';
-import useCustomQuery from '@/app/_hooks/useCustomQuery';
+import { useInView } from 'react-intersection-observer';
+import { useEffect } from 'react';
 import Icons from '../../ui/Icon';
 import { XIcon } from '../../ui/iconPath';
 import { deleteNotification, fetchNotifications, putNotification } from '@/app/_services/notificationsService';
 import { NotificationItem } from '../_types/notifications';
+import useCustomInfiniteQuery from '@/app/_hooks/useCustomInfiniteQuery';
 
 export default function Alarm() {
   const queryClient = useQueryClient();
-  const LAST_ID = 15;
 
-  const { data: notifications } = useCustomQuery(['notifications'], () => fetchNotifications(String(LAST_ID)));
-  console.log('알림', notifications);
-  const handleAllClick = () => {
-    alert('모두 지우기 클릭');
-  };
+  // 무한 스크롤
+  const { ref, inView } = useInView();
+  const { data, fetchNextPage, hasNextPage, isFetchingNextPage } = useCustomInfiniteQuery(
+    ['notifications'],
+    ({ pageParam = '0' }) => fetchNotifications(String(pageParam)),
+    {
+      getNextPageParam: (lastPage) => {
+        if (lastPage.data.lastId === -1) return undefined;
+        return lastPage.data.lastId;
+      },
+      initialPageParam: '0',
+    },
+  );
 
+  // 무한 스크롤 동작
+  useEffect(() => {
+    if (inView && hasNextPage && !isFetchingNextPage) {
+      fetchNextPage();
+    }
+  }, [inView, hasNextPage, isFetchingNextPage, fetchNextPage]);
+
+  // 알림 읽음 처리
   const handleRead = async (notificationId: string) => {
     try {
       await putNotification(notificationId); // API 호출
@@ -24,6 +41,8 @@ export default function Alarm() {
       console.error('Error read notification:', error);
     }
   };
+
+  // 알림 삭제
   const handleDelete = async (notificationId: string) => {
     try {
       await deleteNotification(notificationId); // API 호출
@@ -34,23 +53,18 @@ export default function Alarm() {
     }
   };
 
+  const notifications = data?.pages.flatMap((page) => page.data.notifications) || [];
+
   return (
     <div className="absolute bg-white-0 border-2 w-80 h-[330px] -translate-x-72 mt-2 rounded-xl overflow-y-auto hide-scrollbar">
       <div className="flex items-center py-1">
         <div className="flex h-4 font-semibold text-sm text-black-0 ml-5 my-3">알림</div>
-        <button
-          type="button"
-          className="absolute font-normal text-[10px] right-3 bg-gray-4 hover:bg-gray-300 hover:text-white-0 text-gray-0 rounded-md px-4 py-[2px]"
-          onClick={handleAllClick}
-        >
-          모두 지우기
-        </button>
       </div>
       <div className="flex-col w-full px-2">
-        {notifications?.data?.notifications.map((item: NotificationItem) => (
+        {notifications?.map((item: NotificationItem) => (
           <div key={item.notificationId} className="flex w-72 h-16 mx-auto mb-3">
             <div className="flex justify-between items-start hover:bg-gray-50 bg-gray-4 text-black-0 text-xs font-semibold rounded-lg w-full h-full px-2">
-              <div className="flex items-center h-12 w-[260px]">
+              <div className="flex items-center h-12 w-[230px]">
                 <div className={`rounded-xl w-[7px] h-[7px] mx-2 ${item.isRead ? 'bg-gray-3' : 'bg-green-0'}`} />
                 <button
                   type="button"
@@ -58,7 +72,7 @@ export default function Alarm() {
                     e.stopPropagation();
                     await handleRead(String(item.notificationId));
                   }}
-                  className="hover:text-gray-0"
+                  className="hover:text-gray-0 "
                 >
                   {item.title}
                 </button>
@@ -77,6 +91,8 @@ export default function Alarm() {
           </div>
         ))}
       </div>
+      <div ref={ref} className="h-1" />
+      {isFetchingNextPage && <div className="w-full h-48 bg-gray-2 rounded-md animate-pulse" />}
     </div>
   );
 }
