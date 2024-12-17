b4cfcfb2600b35f7253966b74d4613dd7055340e
jiyeon <bjyy0201@gmail.com>
2024-12-17 17:25:23 +0900

    💄 design: [모의면접]: 표정 분석 결과 메세지 수정
    KAN-274

--- src/app/(route)/(interview)/interview/[username]/[corp]/chat/_types/messageType.ts
+++ src/app/(route)/(interview)/interview/[username]/[corp]/chat/_types/messageType.ts
@@ -6,12 +6,12 @@ export interface InterviewMessages {
 
 // 표정분석 결과
 export const expressionMapping: { [key: string]: string } = {
-  Angry: '화난 표정',
-  Disgust: '역겨운 표정',
-  Fear: '두려운 표정',
-  Happy: '행복한 표정',
-  Sad: '슬픈 표정',
-  Surprise: '놀란 표정',
-  Neutral: '무표정',
-  'No face detected': '얼굴이 인식되지 않음',
+  Angry: '밝은 표정을 지어도 좋을 것 같아요',
+  Disgust: '인상을 풀고 편안한 표정을 지어보세요',
+  Fear: '많이 긴장되어 보여요 좀 더 미소 지어도 좋을 것 같아요',
+  Happy: '적당한 미소 정도가 좋아요',
+  Sad: '슬픈 표정보다 은은한 미소가 좋을 것 같아요',
+  Surprise: '너무 놀란 표정은 당황스러워 보일 수 있어요',
+  Neutral: '좋아요!',
+  'No face detected': '얼굴이 인식되지 않아요',
 };
